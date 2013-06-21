2afa14220c0ebfc6689646fe05a74eceaeb7dcda
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-06-21 21:20:11 +0300

    fixed typo

--- PersonalMessages/Gui/Widgets/MessagesPanel.php
+++ PersonalMessages/Gui/Widgets/MessagesPanel.php
@@ -197,8 +197,8 @@ class MessagesPanel extends \ManiaLive\Gui\Window {
             $targetPlayer = $this->storage->getPlayerObject($target);
             $sourcePlayer = $this->storage->getPlayerObject($login);
             \ManiaLivePlugins\eXpansion\PersonalMessages\PersonalMessages::$reply[$login] = $target;
-            $this->connection->chatSendServerMessage('$faf' . $sourcePlayer->nickName . '$z$s$faf->' . $targetPlayer->nickName . '$z$s$faf : ' . $this->message, $login);
-            $this->connection->chatSendServerMessage('$faf' . $sourcePlayer->nickName . '$z$s$faf->' . $targetPlayer->nickName . '$z$s$faf : ' . $this->message, $target);
+            $this->connection->chatSendServerMessage('$faf' . $sourcePlayer->nickName . '$z$s$faf->' . $targetPlayer->nickName . '$z$s$faf : ' . $args['message'], $login);
+            $this->connection->chatSendServerMessage('$faf' . $sourcePlayer->nickName . '$z$s$faf->' . $targetPlayer->nickName . '$z$s$faf : ' . $args['message'], $target);
         } catch (\Exception $e) {
             $this->connection->chatSendServerMessage('$f00$oError $z$s$fff' . $e->getMessage(), $login);
         }
