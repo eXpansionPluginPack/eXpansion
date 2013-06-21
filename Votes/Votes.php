f0cec620011487fc984e89513f71689663c436be
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-06-21 17:44:29 +0300

    fixed votes

--- Votes/Votes.php
+++ Votes/Votes.php
@@ -13,15 +13,27 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
     private $config;
     private $useQueue = false;
     private $timer = 0;
-    private $defTimeOut;
     private $voter = null;
     private $debug = true;
 
     function exp_onInit() {
 
         $this->config = Config::getInstance();
+        $ratios = $this->connection->getCallVoteRatios();
+        foreach ($ratios as $ratio) {
+            if ($ratio['Command'] == "*") {
+                $this->config->restartVote_ratio = floatval($ratio["Ratio"]);
+                $this->config->skipVote_ratio = floatval($ratio["Ratio"]);
+            }
+            if ($ratio['Command'] == "RestartMap")
+                $this->config->restartVote_ratio = floatval($ratio["Ratio"]);
+            if ($ratio['Command'] == "NextMap")
+                $this->config->skipVote_ratio = floatval($ratio["Ratio"]);
+        }
 
-        //Oliverde8 Menu
+        if ($this->config->restartVote_ratio == -1)
+            $this->config->restartVote_ratio = 1;
+//Oliverde8 Menu
         if ($this->isPluginLoaded('oliverde8\HudMenu'))
             Dispatcher::register(\ManiaLivePlugins\oliverde8\HudMenu\onOliverde8HudMenuReady::getClass(), $this);
     }
@@ -29,17 +41,17 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
     public function exp_onLoad() {
         parent::exp_onLoad();
 
-        $this->enableStorageEvents();
         $this->enableDedicatedEvents();
 
         if ($this->config->restartVote_enable) {
-            $this->registerChatCommand("restartmap", "vote_Restart", 0, true);
+            $this->registerChatCommand("replay", "vote_Restart", 0, true);
+            $this->registerChatCommand("restart", "vote_Restart", 0, true);
         }
         if ($this->config->skipVote_enable) {
-            $this->registerChatCommand("skipmap", "vote_Skip", 0, true);
+            $this->registerChatCommand("skip", "vote_Skip", 0, true);
         }
-        //$cmd = $this->addAdminCommand("poll", $this, "vote_Poll", null);
-        //$cmd->setHelp("Run a yes/no vote with the entered question");
+//$cmd = $this->addAdminCommand("poll", $this, "vote_Poll", null);
+//$cmd->setHelp("Run a yes/no vote with the entered question");
 
         $cmd = AdminGroups::addAdminCommand('cancelvote', $this, 'cancelVote', 'cancel_vote');
         $cmd->setHelp('Cancel current running callvote');
@@ -53,26 +65,29 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
 
         if ($this->isPluginLoaded('eXpansion\Maps') && $this->config->restartVote_useQueue) {
             $this->useQueue = true;
-            if ($this->debug) echo "[exp\Votes] Restart votes set to queue\n";
+            if ($this->debug)
+                echo "[exp\Votes] Restart votes set to queue\n";
         } else {
-            if ($this->debug) echo "[exp\Votes] Restart vote set to normal\n";
+            if ($this->debug)
+                echo "[exp\Votes] Restart vote set to normal\n";
         }
 
-        /*if ($this->config->defaultVotes_disable) {
-            $this->defTimeOut = $this->connection->getCallVoteTimeOut();
-            $this->defTimeOut = $this->defTimeOut['CurrentValue'];
-            if ($this->debug) echo "[exp\Votes] Disabling default timeout (".$this->defTimeOut.")... ";
-            try {
-                $this->connection->setCallVoteTimeOut(1);  // can't set to 0..  disallows custom votes..  :\
-            } catch (\Exception $e) {
-                $this->exp_chatSendServerMessage(__("Error: %s", $login, $e->getMessage()));
-                if ($this->debug) echo "failed.";
-            }
-            if ($this->debug) echo "\n";
-        }*/
+        /* if ($this->config->defaultVotes_disable) {
+          $this->defTimeOut = $this->connection->getCallVoteTimeOut();
+          $this->defTimeOut = $this->defTimeOut['CurrentValue'];
+          if ($this->debug) echo "[exp\Votes] Disabling default timeout (".$this->defTimeOut.")... ";
+          try {
+          $this->connection->setCallVoteTimeOut(1);  // can't set to 0..  disallows custom votes..  :\
+          } catch (\Exception $e) {
+          $this->exp_chatSendServerMessage(__("Error: %s", $login, $e->getMessage()));
+          if ($this->debug) echo "failed.";
+          }
+          if ($this->debug) echo "\n";
+          } */
 
         if ($this->isPluginLoaded('eXpansion\Menu')) {
-            if ($this->debug) echo "[exp\Votes] Building eXp\Menu buttons..\n";
+            if ($this->debug)
+                echo "[exp\Votes] Building eXp\Menu buttons..\n";
             if ($this->config->restartVote_enable || $this->config->skipVote_enable) {
                 $this->callPublicMethod('eXpansion\Menu', 'addSeparator', __('Votes'), false);
             }
@@ -86,22 +101,24 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
         }
 
         if ($this->isPluginLoaded('Standard\Menubar')) {
-            if ($this->debug) echo "[exp\Votes] Building Standard Menubar buttons..";
+            if ($this->debug)
+                echo "[exp\Votes] Building Standard Menubar buttons..";
             $this->callPublicMethod('Standard\Menubar', 'addButton', 'Restart Map Vote', array($this, 'vote_Restart'), false);
             $this->callPublicMethod('Standard\Menubar', 'addButton', 'Skip Map Vote', array($this, 'vote_Skip'), false);
             $this->callPublicMethod('Standard\Menubar', 'addButton', 'Cancel Vote', array($this, 'cancelVote'), true);
         }
     }
 
-    public function onBeginMatch () {
+    public function onBeginMatch() {
 
         $this->timer = time();
-        if ($this->debug) echo "[exp\Votes] Timer set..\n";
+        if ($this->debug)
+            echo "[exp\Votes] Timer set..\n";
     }
 
-    public function onEndMap ($rankings, $map, $wasWarmUp, $matchContinuesOnNextMap, $restartMap) {
+    public function onEndMap($rankings, $map, $wasWarmUp, $matchContinuesOnNextMap, $restartMap) {
 
-        //$this->connection->cancelVote();
+//$this->connection->cancelVote();
     }
 
     /**
@@ -110,7 +127,8 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
      */
     public function onOliverde8HudMenuReady($menu) {
 
-        if ($this->debug) echo "[exp\Votes] Building Oliverde8Hud submenu..\n";
+        if ($this->debug)
+            echo "[exp\Votes] Building Oliverde8Hud submenu..\n";
 
         if ($this->config->restartVote_enable || $this->config->skipVote_enable) {
 
@@ -141,7 +159,7 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
         $this->hudMenuAdminButtons($menu);
     }
 
-    private function hudMenuAdminButtons($menu){
+    private function hudMenuAdminButtons($menu) {
 
         $parent = $menu->findButton(array('admin', 'Basic Commands'));
         if (!$parent) {
@@ -155,40 +173,35 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
         $menu->addButton($parent, "Cancel Vote", $button);
     }
 
-    public function vote_Restart ($login) {
+    public function vote_Restart($login) {
 
         $this->voter = $login;
 
-        if ($this->useQueue) {
-            if ($this->debug) echo "[exp\Votes] Calling Restart (queue) vote..\n";
-            $vote = new \DedicatedApi\Structures\Vote();
-            $vote->callerLogin = $this->voter;
-            $vote->cmdName = "Replay";
-            $vote->cmdParam = array("the current map");
-            $this->connection->callVote($vote, $this->config->restartVote_ratio, ($this->config->restartVote_timeout * 1000), $this->config->restartVote_voters);
-        } else {
-            if ($this->debug) echo "[exp\Votes] Calling Restart vote..\n";
-            $this->connection->callVoteRestartMap($this->config->restartVote_ratio, ($this->config->restartVote_timeout * 1000), $this->config->restartVote_voters);
-        }
+
+        if ($this->debug)
+            echo "[exp\Votes] Calling Restart (queue) vote..\n";
+        $vote = new \DedicatedApi\Structures\Vote();
+        $vote->callerLogin = $this->voter;
+        $vote->cmdName = "Replay";
+        $vote->cmdParam = array("the current map");
+        $this->connection->callVote($vote, $this->config->restartVote_ratio, ($this->config->restartVote_timeout * 1000), $this->config->restartVote_voters);
+
 
         $player = $this->storage->getPlayerObject($login);
         $msg = exp_getMessage('#variable#%1$s #rank#initiated restart map vote..');
         $this->exp_chatSendServerMessage($msg, null, array(\ManiaLib\Utils\Formatting::stripCodes($player->nickName, 'wosnm')));
     }
 
-    public function vote_Skip ($login) {
+    public function vote_Skip($login) {
 
-        if (!$this->config->skipVote_limit) {
-        } else {
-            if (time() > $this->timer + $this->config->skipVote_limit) {
-                $msg = exp_getMessage('#admin_error# $iMust run skip vote within first %1$s seconds of match start..');
-                $this->exp_chatSendServerMessage($msg, $login, array($this->config->skipVote_limit));
-                return;
-            }
-        }
         $this->voter = $login;
-        if ($this->debug) echo "[exp\Votes] Calling skip vote..\n";
-        $this->connection->callVoteNextMap($this->config->skipVote_ratio, ($this->config->skipVote_timeout * 1000), $this->config->skipVote_voters);
+        if ($this->debug)
+            echo "[exp\Votes] Calling skip vote..\n";
+        $vote = new \DedicatedApi\Structures\Vote();
+        $vote->callerLogin = $this->voter;
+        $vote->cmdName = "Skip";
+        $vote->cmdParam = array("the current map");
+        $this->connection->callVote($vote, $this->config->skipVote_ratio, ($this->config->skipVote_timeout * 1000), $this->config->skipVote_voters);
 
         $player = $this->storage->getPlayerObject($login);
         $msg = exp_getMessage('#variable#%1$s #rank#initiated skip map vote..');
@@ -197,51 +210,72 @@ class Votes extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
 
     public function onVoteUpdated($stateName, $login, $cmdName, $cmdParam) {
 
-        if ($this->debug) echo "[exp\Votes] Vote Status: " . $stateName . " -> " . $login . " -> " . $cmdName . " -> " . $cmdParam . "\n";
+        if ($this->debug)
+            echo "[exp\Votes] Vote Status: " . $stateName . " -> " . $login . " -> " . $cmdName . " -> " . $cmdParam . "\n";
 
-        switch ($cmdName) {
-            case "Replay":
-                switch ($stateName) {
-                    case "VotePassed":
-                        $msg = exp_getMessage('#record# $iVote passed!');
-                        $this->exp_chatSendServerMessage($msg, null);
-                        $this->callPublicMethod('eXpansion\\Maps', 'replayMap', $this->voter);
-                        $this->voter = null;
-                        break;
-                    case "VoteFailed":
-                        $msg = exp_getMessage('#admin_error# $iVote failed!');
-                        $this->exp_chatSendServerMessage($msg, null);
-                        $this->voter = null;
-                        break;
-                    default:
-                        break;
-                }
-                break;
-            default:
-                break;
+// disable default votes... and replace them with our own implementations
+        if ($stateName == "NewVote") {
+            if ($cmdName == "RestartMap") {
+                $this->connection->cancelVote();
+                $this->vote_Restart($login);
+                return;
+            }
+            if ($cmdName == "SkipMap") {
+                $this->connection->cancelVote();
+                $this->vote_Skip($login);
+                return;
+            }
+        }
+
+
+// own votes handling...
+
+        if ($stateName == "VotePassed") {
+            if ($cmdName != "Replay" && $cmdName != "Skip")
+                return;
+
+            $msg = exp_getMessage('#record# $iVote passed!');
+            $this->exp_chatSendServerMessage($msg, null);
+            $voter = $this->voter;
+            if ($cmdName == "Replay") {
+                $this->callPublicMethod('eXpansion\\Maps', 'replayMap', $voter);
+            }
+            if ($cmdName == "Skip") {
+                $this->connection->nextMap();
+            }
+            $this->voter = null;
+        }
+        if ($stateName == "VoteFailed") {
+            if ($cmdName != "Replay" && $cmdName != "Skip")
+                return;
+            $msg = exp_getMessage('#admin_error# $iVote failed!');
+            $this->exp_chatSendServerMessage($msg, null);
+            $this->voter = null;
         }
     }
 
-    function cancelVote ($login) {
+    function cancelVote($login) {
         $player = $this->storage->getPlayerObject($login);
         $this->connection->cancelVote();
         $msg = exp_getMessage('#admin_action#Admin #variable#%1$s #admin_action# cancelled the vote!');
         $this->exp_chatSendServerMessage($msg, null, array(\ManiaLib\Utils\Formatting::stripCodes($player->nickName, 'wosnm'), $login));
     }
 
-    public function onUnload() {
+    public
+
+    function onUnload() {
         parent::onUnload();
 
-        /*if ($this->config->defaultVotes_disable) {
-            if ($this->debug) echo "[exp\Votes] Resetting CallVote Timeout (".$this->defTimeOut.")... ";
-            try {
-                $this->connection->setCallVoteTimeOut($this->defTimeOut);
-            } catch (\Exception $e) {
-                if ($this->debug) echo "failed.";
-                $this->exp_chatSendServerMessage(__("Error: %s", $login, $e->getMessage()));
-            }
-            if ($this->debug) echo "\n";
-        }*/
+        /* if ($this->config->defaultVotes_disable) {
+          if ($this->debug) echo "[exp\Votes] Resetting CallVote Timeout (".$this->defTimeOut.")... ";
+          try {
+          $this->connection->setCallVoteTimeOut($this->defTimeOut);
+          } catch (\Exception $e) {
+          if ($this->debug) echo "failed.";
+          $this->exp_chatSendServerMessage(__("Error: %s", $login, $e->getMessage()));
+          }
+          if ($this->debug) echo "\n";
+          } */
     }
 
 }
