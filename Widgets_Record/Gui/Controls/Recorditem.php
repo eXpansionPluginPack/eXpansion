c79f91a840914752a80c58e57ca23c3c7ab985b6
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-06-22 15:42:50 +0300

    finally optimized the record widget.. note: don't do too many frames inside frames, parsing it on gameclient may cause fps drop

--- Widgets_Record/Gui/Controls/Recorditem.php
+++ Widgets_Record/Gui/Controls/Recorditem.php
@@ -35,48 +35,46 @@ class Recorditem extends \ManiaLive\Gui\Control {
           $this->addComponent($this->bg);
           } */
 
-        $this->frame = new \ManiaLive\Gui\Controls\Frame();
-        $this->frame->setSize($sizeX, $sizeY);
-        $this->frame->setLayout(new \ManiaLib\Gui\Layouts\Line());
+        //  $this->frame = new \ManiaLive\Gui\Controls\Frame();
+        //   $this->frame->setSize($sizeX, $sizeY);
+        //   $this->frame->setLayout(new \ManiaLib\Gui\Layouts\Line());
 
-        $this->label = new \ManiaLib\Gui\Elements\Label(4, 4);
+        $this->label = new \ManiaLib\Gui\Elements\Label(4, 1);
         $this->label->setAlign('center', 'center');
+        $this->label->setPosition(0,0.5);
         $this->label->setScale(0.7);
         $bold = "";
         if ($index <= 3)
             $bold = '$o';
         $this->label->setText('$fff' . $bold . $index);
-        $this->frame->addComponent($this->label);
+        $this->addComponent($this->label);
 
-        $spacer = new \ManiaLib\Gui\Elements\Quad();
-        $spacer->setSize(1, 4);
-        $spacer->setStyle(\ManiaLib\Gui\Elements\Icons64x64_1::EmptyIcon);
-        $this->frame->addComponent($spacer);
+        $flag = new \ManiaLib\Gui\Elements\Quad(3, 3);
+        $flag->setPosition(4,0);
+        $flag->setAlign("left", "center");
+        $flag->setStyle(\ManiaLib\Gui\Elements\Icons64x64_1::EmptyIcon);
 
-        /* $flag = new \ManiaLib\Gui\Elements\Quad(3, 3);
-          $flag->setAlign("left", "center");
-          $flag->setStyle(\ManiaLib\Gui\Elements\Icons64x64_1::EmptyIcon);
+        $config = Config::getInstance();
+        $path = $record->nation;
 
-          $config = Config::getInstance();
-          $path = $record->nation;
+        if ($path !== null) {
+            $path = explode("|", $path);
+            if (sizeof($path) >= 2) {
 
-          if ($path !== null) {
-          $path = explode("|", $path);
-          if (sizeof($path) >= 2) {
-
-          $image = "file://Skins/Avatars/Flags/" . Countries::mapCountry($path[2]) . ".dds";
-          $flag->setStyle("");
-          $flag->setImage($image, false);
-          }
-          }
-          $this->frame->addComponent($flag);
-
+                $image = "file://Skins/Avatars/Flags/" . Countries::mapCountry($path[2]) . ".dds";
+                $flag->setStyle("");
+                $flag->setImage($image, false);
+            }
+        }
+        $this->addComponent($flag);
+        /*
           $spacer = new \ManiaLib\Gui\Elements\Quad();
           $spacer->setSize(1, 4);
           $spacer->setStyle(\ManiaLib\Gui\Elements\Icons64x64_1::EmptyIcon);
           $this->frame->addComponent($spacer);
          */
         $this->label = new \ManiaLib\Gui\Elements\Label(14, 4);
+        $this->label->setPosition(9);
         $this->label->setAlign('left', 'center');
         $this->label->setScale(0.7);
         $this->label->setText('$fff' . \ManiaLive\Utilities\Time::fromTM($record->time));
@@ -84,14 +82,10 @@ class Recorditem extends \ManiaLive\Gui\Control {
             $this->label->setStyle("TextTitle2Blink");
             $this->label->setTextSize(1);
         }
-        $this->frame->addComponent($this->label);
-
-        $spacer = new \ManiaLib\Gui\Elements\Quad();
-        $spacer->setSize(1, 4);
-        $spacer->setStyle(\ManiaLib\Gui\Elements\Icons64x64_1::EmptyIcon);
-        $this->frame->addComponent($spacer);
+        $this->addComponent($this->label);
 
         $this->nick = new \ManiaLib\Gui\Elements\Label(30, 4);
+        $this->nick->setPosition(20,0);
         $this->nick->setAlign('left', 'center');
         $this->nick->setScale(0.7);
 
@@ -102,9 +96,9 @@ class Recorditem extends \ManiaLive\Gui\Control {
 
         $nickname = \ManiaLib\Utils\Formatting::stripCodes($record->nickName, "wosnm");
         $this->nick->setText('$fff' . $nickname);
-        $this->frame->addComponent($this->nick);
+        $this->addComponent($this->nick);
 
-        $this->addComponent($this->frame);
+        // $this->addComponent($this->frame);
 
         $this->sizeX = $sizeX;
         $this->sizeY = $sizeY;
@@ -112,7 +106,7 @@ class Recorditem extends \ManiaLive\Gui\Control {
     }
 
     protected function onResize($oldX, $oldY) {
-        $this->frame->setSize($this->sizeX, $this->sizeY);
+       // $this->frame->setSize($this->sizeX, $this->sizeY);
     }
 
     function onDraw() {
@@ -120,8 +114,8 @@ class Recorditem extends \ManiaLive\Gui\Control {
     }
 
     public function destroy() {
-        $this->frame->clearComponents();
-        $this->frame->destroy();
+        // $this->frame->clearComponents();
+        // $this->frame->destroy();
         $this->clearComponents();
         parent::destroy();
     }
