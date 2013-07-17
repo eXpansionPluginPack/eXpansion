45be43cc0193715dd56faba6bd8a472b3dd4c11c
Petri Järvisalo <petri.jarvisalo@gmail.com>
2013-07-17 10:50:40 +0300

    clock widget can be moveed now

--- Widgets_Clock/Gui/Widgets/Clock.php
+++ Widgets_Clock/Gui/Widgets/Clock.php
@@ -10,6 +10,14 @@ class Clock extends \ManiaLive\Gui\Window {
 
     protected function onConstruct() {
         parent::onConstruct();
+        $move = new \ManiaLib\Gui\Elements\Quad(60, 12);
+        $move->setStyle("Bgs1InRace");
+        $move->setSubStyle("BgList");
+        $move->setBgcolor("0000");        
+        $move->setScriptEvents();
+        $move->setId("enableMove");
+        $this->addComponent($move);
+
 
         $this->clock = new \ManiaLib\Gui\Elements\Label();
         $this->clock->setId('clock');
@@ -21,7 +29,7 @@ class Clock extends \ManiaLive\Gui\Window {
         //$this->clock->setTextPrefix('$s');
         $this->addComponent($this->clock);
 
-        $this->date = new \ManiaLib\Gui\Elements\Label(60,6);
+        $this->date = new \ManiaLib\Gui\Elements\Label(60, 6);
         $this->date->setId('date');
         $this->date->setAlign("left", "top");
         $this->date->setPosition(0, 0);
@@ -30,31 +38,87 @@ class Clock extends \ManiaLive\Gui\Window {
         $this->date->setText(\ManiaLive\Data\Storage::getInstance()->server->name);
         $this->addComponent($this->date);
 
+        $this->setAlign("left", "top");
         $this->xml = new \ManiaLive\Gui\Elements\Xml();
-        $this->xml->setContent('
-              <script><!--
-              #Include "TextLib" as TextLib 
-              
-                       main () {     
-                            declare Window <=> Page.GetFirstChild("' . $this->getId() . '");    
-                            declare CMlLabel lbl_clock <=> (Page.GetFirstChild("clock") as CMlLabel);
-                            declare CMlLabel lbl_date <=> (Page.GetFirstChild("date") as CMlLabel);
-
-                            while(True) { 
-                                  // lbl_date.SetText(""^TextLib::SubString(CurrentLocalDateText, 8, 2)^"."^TextLib::SubString(CurrentLocalDateText, 5, 2)^"."^TextLib::SubString(CurrentLocalDateText, 0, 4) );
-                                   lbl_clock.SetText(""^TextLib::SubString(CurrentLocalDateText, 11, 2)^":"^TextLib::SubString(CurrentLocalDateText, 14, 2)^":"^TextLib::SubString(CurrentLocalDateText, 17, 2));
-                            yield;
-                            }
-                        }
-                        -->
-                        </script>
-                        
-');
-        $this->addComponent($this->xml);
     }
 
-    function onDraw() {
+    public function onDraw() {
+        $this->removeComponent($this->xml);
+        $this->xml->setContent('    
+        <script><!--
+        #Include "TextLib" as TextLib
         
+                       main () {     
+                        declare Window <=> Page.GetFirstChild("' . $this->getId() . '");                 
+                        declare MoveWindow = False;                       
+                        declare CMlLabel lbl_clock <=> (Page.GetFirstChild("clock") as CMlLabel);
+                        declare CMlLabel lbl_date <=> (Page.GetFirstChild("date") as CMlLabel);
+               
+                        declare Vec3 LastDelta = <Window.RelativePosition.X, Window.RelativePosition.Y, 0.0>;
+                        declare Vec3 DeltaPos = <0.0, 0.0, 0.0>;
+                        declare Real lastMouseX = 0.0;
+                        declare Real lastMouseY =0.0;                           
+                        declare Text id = "Clock";      
+                        
+                        declare persistent Vec3[Text] windowLastPos;
+                        declare persistent Vec3[Text] windowLastPosRel;
+                        
+                        
+                         if (!windowLastPos.existskey(id)) {
+                                windowLastPos[id] = <-159.0, 89.0, 0.0>;
+                               }
+                         if (!windowLastPosRel.existskey(id)) {
+                                windowLastPosRel[id] = <-159.0, 89.0, 0.0>;
+                                }
+                        Window.PosnX = windowLastPos[id][0];
+                        Window.PosnY = windowLastPos[id][1];
+                        LastDelta = windowLastPosRel[id];
+                        Window.RelativePosition = windowLastPosRel[id];                                                
+                        
+                        while(True) {                                                               
+                       
+                                if (MoveWindow) {                                                                                                    
+                                    DeltaPos.X = MouseX - lastMouseX;
+                                    DeltaPos.Y = MouseY - lastMouseY;
+                                   
+                                    LastDelta += DeltaPos;
+                                    LastDelta.Z = 3.0;
+                                    Window.RelativePosition = LastDelta;                                
+                                    windowLastPos[id] = Window.AbsolutePosition;
+                                    windowLastPosRel[id] = Window.RelativePosition;
+                                    
+                                    lastMouseX = MouseX;
+                                    lastMouseY = MouseY;                            
+                                    }
+                                    
+                                    if (MouseMiddleButton == True) {
+                                
+                                    foreach (Event in PendingEvents) {
+                                 
+                                        
+                                     
+                                              foreach (Event in PendingEvents) {
+                                                      if (Event.ControlId == "enableMove") {                                                         
+                                                            lastMouseX = MouseX;
+                                                            lastMouseY = MouseY;   
+                                                            MoveWindow = True;                                                           
+                                                        }                                                                                                  
+                                                }
+                                        }
+                                    
+                                    }
+                                        else {
+                                            MoveWindow = False;                                                                          
+                                        }
+                                lbl_clock.SetText(""^TextLib::SubString(CurrentLocalDateText, 11, 2)^":"^TextLib::SubString(CurrentLocalDateText, 14, 2)^":"^TextLib::SubString(CurrentLocalDateText, 17, 2));
+                                yield;                        
+                            }
+                  
+                  
+                } 
+                --></script>');
+        $this->addComponent($this->xml);
+        parent::onDraw();
     }
 
     function destroy() {
