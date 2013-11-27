89c16791c924a8734f86c2659eec285e40937f2f
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-11-27 16:01:22 +0200

    adding info for newcomers to press TAB

--- JoinLeaveMessage/JoinLeaveMessage.php
+++ JoinLeaveMessage/JoinLeaveMessage.php
@@ -6,12 +6,15 @@ class JoinLeaveMessage extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin 
 
     private $joinMsg;
     private $leaveMsg;
+    private $tabNoticeMsg;
 
     public function exp_onLoad() {
 	$this->enableDedicatedEvents();
 
+
 	$this->joinMsg = exp_getMessage('#player#Player %s$1 #player# (#variable#%s$2#player#) from #variable#%s$3 #player# joins! #variable#%s$4');
 	$this->leaveMsg = exp_getMessage('#player#Player %s$1 #player# (#variable#%s$2#player#) leaves!');
+	$this->tabNoticeMsg = exp_getMessage('#variable#[Info] Some widgets are hidden from the main view, press TAB to show them. Use F8 to disable all custom server graphics.');
     }
 
     public function exp_unload() {
@@ -30,6 +33,7 @@ class JoinLeaveMessage extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin 
 		$spec = __("\$n(Spectator)", $login);
 
 	    $this->exp_chatSendServerMessage($this->joinMsg, null, array($nick, $login, $country, $spec));
+	    $this->exp_chatSendServerMessage($this->tabNoticeMsg, $login);
 	} catch (\Exception $e) {
 	    echo $e->getLine() . ":" . $e->getMessage();
 	}
