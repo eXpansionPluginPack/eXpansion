3408ef1c6d5a5749d67f72aa8a5ff111d5304458
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-11-30 23:30:45 +0200

    Added new commands /play and /spec

--- Players/Gui/Windows/Playerlist.php
+++ Players/Gui/Windows/Playerlist.php
@@ -13,148 +13,157 @@ use ManiaLivePlugins\eXpansion\AdminGroups\AdminGroups;
 class Playerlist extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window {
 
     private $pager;
+
+    /** @var \DedicatedApi\Connection */
     private $connection;
+
+    /** @var \ManiaLive\Data\Storage */
     private $storage;
     private $items = array();
 
     protected function onConstruct() {
-        parent::onConstruct();
-        $config = \ManiaLive\DedicatedApi\Config::getInstance();
-        $this->connection = \DedicatedApi\Connection::factory($config->host, $config->port);
-        $this->storage = \ManiaLive\Data\Storage::getInstance();
+	parent::onConstruct();
+	$config = \ManiaLive\DedicatedApi\Config::getInstance();
+	$this->connection = \DedicatedApi\Connection::factory($config->host, $config->port);
+	$this->storage = \ManiaLive\Data\Storage::getInstance();
 
-        $this->pager = new \ManiaLive\Gui\Controls\Pager();
-        $this->mainFrame->addComponent($this->pager);
+	$this->pager = new \ManiaLive\Gui\Controls\Pager();
+	$this->mainFrame->addComponent($this->pager);
     }
 
     function ignorePlayer($login, $target) {
-        try {
-            $login = $this->getRecipient();
-            if (!AdminGroups::hasPermission($login, 'player_ignore')) {
-                $this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
-            }
-            $player = $this->storage->getPlayerObject($target);
-            $admin = $this->storage->getPlayerObject($login);
-            $this->connection->ignore($target);
-            $this->connection->chatSendServerMessage(__('$%sz was ignored by admin.', $player->nickName));
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
-        }
+	try {
+	    $login = $this->getRecipient();
+	    if (!AdminGroups::hasPermission($login, 'player_ignore')) {
+		$this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
+	    }
+	    $player = $this->storage->getPlayerObject($target);
+	    $admin = $this->storage->getPlayerObject($login);
+	    $this->connection->ignore($target);
+	    $this->connection->chatSendServerMessage(__('$%sz was ignored by admin.', $player->nickName));
+	} catch (\Exception $e) {
+	    $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
+	}
     }
 
     function kickPlayer($login, $target) {
-        try {
-            $login = $this->getRecipient();
-            if (!AdminGroups::hasPermission($login, 'player_kick')) {
-                $this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
-            }
-            $player = $this->storage->getPlayerObject($target);
-            $admin = $this->storage->getPlayerObject($login);
-            $this->connection->kick($target, __("Please behave next time you visit the server!"));
-            $this->connection->chatSendServerMessage(__('$%sz was kicked from the server by admin.', $player->nickName));
-            // can't use notice...since $this->storage->players too slow.
-            // $this->connection->sendNotice($this->storage->players, $player->nickName . '$z were kicked from the server by admin.');
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
-        }
+	try {
+	    $login = $this->getRecipient();
+	    if (!AdminGroups::hasPermission($login, 'player_kick')) {
+		$this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
+	    }
+	    $player = $this->storage->getPlayerObject($target);
+	    $admin = $this->storage->getPlayerObject($login);
+	    $this->connection->kick($target, __("Please behave next time you visit the server!"));
+	    $this->connection->chatSendServerMessage(__('$%sz was kicked from the server by admin.', $player->nickName));
+	    // can't use notice...since $this->storage->players too slow.
+	    // $this->connection->sendNotice($this->storage->players, $player->nickName . '$z were kicked from the server by admin.');
+	} catch (\Exception $e) {
+	    $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
+	}
     }
 
     function banPlayer($login, $target) {
-        try {
-            $login = $this->getRecipient();
-            if (!AdminGroups::hasPermission($login, 'player_ban')) {
-                $this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
-            }
-            $player = $this->storage->getPlayerObject($target);
-            $admin = $this->storage->getPlayerObject($login);
-            $this->connection->ban($target, __("You are now banned from the server."));
-            $this->connection->chatSendServerMessage(__('%s$z has been banned from the server.', $player->nickName));
-            //$this->connection->sendNotice($this->storage->players, $player->nickName . '$z has been banned from the server.');
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
-        }
+	try {
+	    $login = $this->getRecipient();
+	    if (!AdminGroups::hasPermission($login, 'player_ban')) {
+		$this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
+	    }
+	    $player = $this->storage->getPlayerObject($target);
+	    $admin = $this->storage->getPlayerObject($login);
+	    $this->connection->ban($target, __("You are now banned from the server."));
+	    $this->connection->chatSendServerMessage(__('%s$z has been banned from the server.', $player->nickName));
+	    //$this->connection->sendNotice($this->storage->players, $player->nickName . '$z has been banned from the server.');
+	} catch (\Exception $e) {
+	    $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
+	}
     }
 
     function blacklistPlayer($login, $target) {
-        try {
-            $login = $this->getRecipient();
-            if (!AdminGroups::hasPermission($login, 'player_black')) {
-                $this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
-            }
-            $player = $this->storage->getPlayerObject($target);
-            $admin = $this->storage->getPlayerObject($login);
-            $this->connection->banAndBlackList($target, __("You are now blacklisted from the server."), true);
-            $this->connection->chatSendServerMessage(__('%s$z has been blacklisted from the server.', $player->nickName));
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
-        }
+	try {
+	    $login = $this->getRecipient();
+	    if (!AdminGroups::hasPermission($login, 'player_black')) {
+		$this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
+	    }
+	    $player = $this->storage->getPlayerObject($target);
+	    $admin = $this->storage->getPlayerObject($login);
+	    $this->connection->banAndBlackList($target, __("You are now blacklisted from the server."), true);
+	    $this->connection->chatSendServerMessage(__('%s$z has been blacklisted from the server.', $player->nickName));
+	} catch (\Exception $e) {
+	    $this->connection->chatSendServerMessage(__("Error: %s", $e->getMessage()));
+	}
     }
 
     function toggleSpec($login, $target) {
-        try {
-            $login = $this->getRecipient();
-            if (!AdminGroups::hasPermission($login, 'player_spec')) {
-                $this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
-            }
-            $player = $this->storage->getPlayerObject($target);
-            if ($player->forceSpectator == 0) {
-                $this->connection->forceSpectator($target, 1);
-                $this->connection->sendNotice($target, __('Admin has forced you to specate!', $login));
-            }
-            if ($player->forceSpectator == 1) {
-                $this->connection->forceSpectator($target, 0);
-                $this->connection->sendNotice($target, __("Admin has released you from specate to play.", $login));
-            }
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__("Error: %s", $login, $e->getMessage()), $login);
-        }
+	try {
+	    $login = $this->getRecipient();
+	    if (!AdminGroups::hasPermission($login, 'player_spec')) {
+		$this->connection->chatSendServerMessage(__('$ff3$iYou are not allowed to do that!'), $login);
+	    }
+	    $player = $this->storage->getPlayerObject($target);
+
+	    if ($player->forceSpectator == 0 || $player->isSpectator == false) {
+		$this->connection->forceSpectator($target, 1);
+		$this->connection->chatSendServerMessage(__('Admin has forced you to specate!', $login), $login);
+		return;
+	    }
+	    if ($player->forceSpectator == 1 || $player->isSpectator == true) {
+		$this->connection->forceSpectator($target, 2);
+		$this->connection->forceSpectator($target, 0);
+		$this->connection->chatSendServerMessage(__("Admin has released you from specate to play.", $login), $login);
+		return;
+	    }
+	} catch (\Exception $e) {
+	    echo $e->getMessage();
+	    $this->connection->chatSendServerMessage(__("Error: %s", $login, $e->getMessage()), $login);
+	}
     }
 
     function onResize($oldX, $oldY) {
-        parent::onResize($oldX, $oldY);
-        $this->pager->setSize($this->sizeX - 2, $this->sizeY - 8);
-        $this->pager->setStretchContentX($this->sizeX);
-        $this->pager->setPosition(4, -4);
+	parent::onResize($oldX, $oldY);
+	$this->pager->setSize($this->sizeX - 2, $this->sizeY - 8);
+	$this->pager->setStretchContentX($this->sizeX);
+	$this->pager->setPosition(4, -4);
     }
 
     function onShow() {
-        $this->populateList();
+	$this->populateList();
     }
 
     function populateList() {
 
-        foreach ($this->items as $item)
-           $item->erase();
-        $this->pager->clearItems();
-        $this->items = array();
-
-        $x = 0;
-        $login = $this->getRecipient();
-        $isadmin = AdminGroups::hasPermission($login, "server_admin");
-        try {
-            foreach ($this->storage->players as $player) {
-                $this->items[$x] = new Playeritem($x++, $player, $this, $isadmin, $this->getRecipient(), $this->sizeX);
-                $this->pager->addItem($this->items[$x]);
-            }
-            foreach ($this->storage->spectators as $player) {
-                $this->items[$x] = new Playeritem($x++, $player, $this, $isadmin, $this->getRecipient(), $this->sizeX);
-                $this->pager->addItem($this->items[$x]);
-            }
-        } catch (\Exception $e) {
-            
-        }
+	foreach ($this->items as $item)
+	    $item->erase();
+	$this->pager->clearItems();
+	$this->items = array();
+	$this->storage = \ManiaLive\Data\Storage::getInstance();
+	$x = 0;
+	$login = $this->getRecipient();
+	$isadmin = AdminGroups::hasPermission($login, "server_admin");
+	try {
+	    foreach ($this->storage->players as $player) {
+		$this->items[$x] = new Playeritem($x++, $player, $this, $isadmin, $this->getRecipient(), $this->sizeX);
+		$this->pager->addItem($this->items[$x]);
+	    }
+	    foreach ($this->storage->spectators as $player) {
+		$this->items[$x] = new Playeritem($x++, $player, $this, $isadmin, $this->getRecipient(), $this->sizeX);
+		$this->pager->addItem($this->items[$x]);
+	    }
+	} catch (\Exception $e) {
+	    
+	}
     }
 
     function destroy() {
-        $this->connection = null;
-        $this->storage = null;
-        foreach ($this->items as $item)
-           $item->erase();
+	$this->connection = null;
+	$this->storage = null;
+	foreach ($this->items as $item)
+	    $item->erase();
 
-        $this->items = null;
+	$this->items = null;
 
 
-        parent::destroy();
+	parent::destroy();
     }
 
 }
--- Players/Players.php
+++ Players/Players.php
@@ -7,89 +7,100 @@ use ManiaLive\Event\Dispatcher;
 class Players extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin {
 
     public function exp_onInit() {
-        parent::exp_onInit();
-        //Oliverde8 Menu
-        if ($this->isPluginLoaded('oliverde8\HudMenu')) {
-            Dispatcher::register(\ManiaLivePlugins\oliverde8\HudMenu\onOliverde8HudMenuReady::getClass(), $this);
-        }
+	parent::exp_onInit();
+	//Oliverde8 Menu
+	if ($this->isPluginLoaded('oliverde8\HudMenu')) {
+	    Dispatcher::register(\ManiaLivePlugins\oliverde8\HudMenu\onOliverde8HudMenuReady::getClass(), $this);
+	}
     }
 
     public function exp_onReady() {
-        $this->enableDedicatedEvents();
-        $this->registerChatCommand("players", "showPlayerList", 0, true); // xaseco
-        $this->registerChatCommand("plist", "showPlayerList", 0, true); // fast
+	$this->enableDedicatedEvents();
+	$this->registerChatCommand("players", "showPlayerList", 0, true); // xaseco
+	$this->registerChatCommand("plist", "showPlayerList", 0, true); // fast
+	$this->registerChatCommand("play", "setPlay", 0, true); // fast
+	$this->registerChatCommand("spec", "setSpec", 0, true); // fast
 
-        if ($this->isPluginLoaded('Standard\Menubar'))
-            $this->buildMenu();
+	if ($this->isPluginLoaded('Standard\Menubar'))
+	    $this->buildMenu();
 
-        if ($this->isPluginLoaded('eXpansion\Menu')) {
-            $this->callPublicMethod('eXpansion\Menu', 'addSeparator', __('Players'), false);
-            $this->callPublicMethod('eXpansion\Menu', 'addItem', __('Show Players'), null, array($this, 'showPlayerList'), false);
-        }
+	if ($this->isPluginLoaded('eXpansion\Menu')) {
+	    $this->callPublicMethod('eXpansion\Menu', 'addSeparator', __('Players'), false);
+	    $this->callPublicMethod('eXpansion\Menu', 'addItem', __('Show Players'), null, array($this, 'showPlayerList'), false);
+	}
     }
 
     public function onOliverde8HudMenuReady($menu) {
-        $parent = $menu->findButton(array("admin", "Players"));
-        $button["plugin"] = $this;
-        if (!$parent) {
-            $button["style"] = "Icons128x128_1";
-            $button["substyle"] = "Profile";
-            $parent = $menu->addButton("admin", "Players", $button);
-        }
+	$parent = $menu->findButton(array("admin", "Players"));
+	$button["plugin"] = $this;
+	if (!$parent) {
+	    $button["style"] = "Icons128x128_1";
+	    $button["substyle"] = "Profile";
+	    $parent = $menu->addButton("admin", "Players", $button);
+	}
 
-        $button["style"] = "Icons128x128_1";
-        $button["substyle"] = "Profile";
-        $button["plugin"] = $this;
-        $button["function"] = 'showPlayerList';
-        $parent = $menu->addButton($parent, "Show Players", $button);
-        
-        $parent = $menu->findButton(array("menu", "Players"));
-        if (!$parent) {
-            $button["style"] = "Icons128x128_1";
-            $button["substyle"] = "Profile";
-            $parent = $menu->addButton("menu", "Players", $button);
-        }
-        
-        $button["style"] = "Icons128x128_1";
-        $button["substyle"] = "Profile";
-        $button["plugin"] = $this;
-        $button["function"] = 'showPlayerList';
-        $parent = $menu->addButton($parent, "Show Players", $button);
+	$button["style"] = "Icons128x128_1";
+	$button["substyle"] = "Profile";
+	$button["plugin"] = $this;
+	$button["function"] = 'showPlayerList';
+	$parent = $menu->addButton($parent, "Show Players", $button);
+
+	$parent = $menu->findButton(array("menu", "Players"));
+	if (!$parent) {
+	    $button["style"] = "Icons128x128_1";
+	    $button["substyle"] = "Profile";
+	    $parent = $menu->addButton("menu", "Players", $button);
+	}
+
+	$button["style"] = "Icons128x128_1";
+	$button["substyle"] = "Profile";
+	$button["plugin"] = $this;
+	$button["function"] = 'showPlayerList';
+	$parent = $menu->addButton($parent, "Show Players", $button);
     }
 
     public function onPlayerDisconnect($login, $reason = null) {
-        \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Erase($login);
-        $this->updateOpenedWindows();
+	\ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Erase($login);
+	$this->updateOpenedWindows();
     }
 
     public function onPlayerConnect($login, $isSpectator) {
-        $this->updateOpenedWindows();        
+	$this->updateOpenedWindows();
     }
 
     public function updateOpenedWindows() {
-        $windows = \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::GetAll();
-        foreach ($windows as $window) {
-            $login = $window->getRecipient();
-            $this->showPlayerList($login);
-        }
+	$windows = \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::GetAll();
+	foreach ($windows as $window) {
+	    $login = $window->getRecipient();
+	    $this->showPlayerList($login);
+	}
+    }
+
+    public function setPlay($login) {
+	$this->connection->forceSpectator($login, 2);
+	$this->connection->forceSpectator($login, 0);
+    }
+
+    public function setSpec($login) {
+	$this->connection->forceSpectator($login, 3);
     }
 
     public function buildMenu() {
-        $this->callPublicMethod('Standard\Menubar', 'initMenu', \ManiaLib\Gui\Elements\Icons64x64_1::Buddy);
-        $this->callPublicMethod('Standard\Menubar', 'addButton', __('Players'), array($this, 'showPlayerList'), false);
+	$this->callPublicMethod('Standard\Menubar', 'initMenu', \ManiaLib\Gui\Elements\Icons64x64_1::Buddy);
+	$this->callPublicMethod('Standard\Menubar', 'addButton', __('Players'), array($this, 'showPlayerList'), false);
     }
 
     public function showPlayerList($login) {
-        \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Erase($login);
-        $window = \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Create($login);
-        $window->setTitle('Players');
-        $window->setSize(160, 100);
-        $window->centerOnScreen();
-        $window->show();
+	\ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Erase($login);
+	$window = \ManiaLivePlugins\eXpansion\Players\Gui\Windows\Playerlist::Create($login);
+	$window->setTitle('Players');
+	$window->setSize(160, 100);
+	$window->centerOnScreen();
+	$window->show();
     }
 
     public function onPlayerInfoChanged($playerInfo) {
-        $this->updateOpenedWindows();
+	$this->updateOpenedWindows();
     }
 
 }
