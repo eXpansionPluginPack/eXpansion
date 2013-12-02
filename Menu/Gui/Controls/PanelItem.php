c343bfa53af0626ded9ad73fc95301ad113102b3
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2013-12-02 16:43:26 +0200

    fixes menu to display localized separator items, fix for english messages

--- Menu/Gui/Controls/PanelItem.php
+++ Menu/Gui/Controls/PanelItem.php
@@ -13,55 +13,55 @@ class PanelItem extends \ManiaLive\Gui\Control {
     private $frame;
 
     function __construct(\ManiaLivePlugins\eXpansion\Menu\Structures\Menuitem $item, $login) {
-        $this->sizeX = 30;
-        $this->sizeY = 5;
-        $this->setAlign("left", "top");
+	$this->sizeX = 30;
+	$this->sizeY = 5;
+	$this->setAlign("left", "top");
 
-        if ($item->isSeparator) {
-            $this->doSeparator($item);
-            return;
-        }
+	if ($item->isSeparator) {
+	    $this->doSeparator($item);
+	    return;
+	}
 
-        $action = \ManiaLive\Gui\ActionHandler::getInstance()->createAction($item->callback);
-        $button = new myButton(40, 6);
-        $button->setScale(0.6);
-        $button->setPosX(4);
-        $button->setText(__($item->title, $login));
-        $button->colorize('0000');
-        $button->setTextColor('fff');
-        $button->setAction($action);
+	$action = \ManiaLive\Gui\ActionHandler::getInstance()->createAction($item->callback);
+	$button = new myButton(40, 6);
+	$button->setScale(0.6);
+	$button->setPosX(4);
+	$button->setText(__($item->title, $login));
+	$button->colorize('0000');
+	$button->setTextColor('fff');
+	$button->setAction($action);
 
-        $this->addComponent($button);
+	$this->addComponent($button);
     }
 
     protected function onResize($oldX, $oldY) {
-       // $this->frame->setSize($this->sizeX, $this->sizeY);
+	// $this->frame->setSize($this->sizeX, $this->sizeY);
     }
 
-    function doSeparator($item) {        
-        $this->sizeX = 30;
-        $this->sizeY = 6;
-        $bg = new \ManiaLib\Gui\Elements\Quad(50, 4);
-        $bg->setPosition(-3,1);        
-        $bg->setAlign("left","top");
-                
-        $bg->setStyle('BgsPlayerCard');
-        $bg->setSubStyle('BgRacePlayerName');
-        $this->addComponent($bg);
-        $label = new \ManiaLib\Gui\Elements\Label(30, 4);
-        $label->setStyle("TextStaticVerySmall");
-        $label->setTextColor('fff');
-        $label->setAlign("left","top");
-        $label->setText($item->title);
-        $this->addComponent($label);
+    function doSeparator($item, $login) {
+	$this->sizeX = 30;
+	$this->sizeY = 6;
+	$bg = new \ManiaLib\Gui\Elements\Quad(50, 4);
+	$bg->setPosition(-3, 1);
+	$bg->setAlign("left", "top");
+
+	$bg->setStyle('BgsPlayerCard');
+	$bg->setSubStyle('BgRacePlayerName');
+	$this->addComponent($bg);
+	$label = new \ManiaLib\Gui\Elements\Label(30, 4);
+	$label->setStyle("TextStaticVerySmall");
+	$label->setTextColor('fff');
+	$label->setAlign("left", "top");
+	$label->setText(__($item->title, $login));
+	$this->addComponent($label);
     }
 
     function onDraw() {
-        
+	
     }
 
     function __destruct() {
-        
+	
     }
 
 }
