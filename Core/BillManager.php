11e9d5ca9a0cd5fbebabebefb2fe8df4875611d4
oliverde8 <oliverde8@gmail.com>
2013-12-17 21:33:18 +0100

    Added begining of the BillManager to centralize coppers transactions in order to save them in the database

---
+++ Core/BillManager.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace ManiaLivePlugins\eXpansion\Core;
+
+/**
+ * Description of BillManager
+ *
+ * @author De Cramer Oliver
+ */
+
+
+class BillManager {
+    //put your code here
+}
+
+?>
