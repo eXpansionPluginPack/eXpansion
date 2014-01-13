5852f54385b75155ddaf5323d6fa1228f0ce85fe
oliverde8 <oliverde8@gmail.com>
2014-01-13 19:21:17 +0100

    Changed default value of config

--- Adm/Config.php
+++ Adm/Config.php
@@ -8,7 +8,7 @@ class Config extends \ManiaLib\Utils\Singleton
 //To deactivate put empty array or -1 in array
 public $publicResAmount = array(20,50, 200);
 
-public $publicSkipAmount = array(5);
+public $publicSkipAmount = array();
 	
 }
 ?>
