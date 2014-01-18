92968cef9cc834d3586775ce375663dd940a6567
Petri Järvisalo <petri.jarvisalo@gmail.com>
2014-01-18 23:31:38 +0200

    better scrollbar gfx

--- Gui/Elements/Pager.php
+++ Gui/Elements/Pager.php
@@ -12,6 +12,7 @@ class Pager extends \ManiaLive\Gui\Control {
     private $pager;
     private $items = array();
     private $scroll;
+    private $scrollBg, $scrollUp, $scrollDown;
 
     public function __construct() {
         $this->pager = new \ManiaLive\Gui\Controls\Frame();
@@ -19,10 +20,29 @@ class Pager extends \ManiaLive\Gui\Control {
         $this->pager->setScriptEvents();
         $this->addComponent($this->pager);
 
-        $this->scroll = new \ManiaLib\Gui\Elements\Quad(6, 20);
-        $this->scroll->setAlign("top", "left");
-        $this->scroll->setStyle("BgsPlayerCard");
-        $this->scroll->setSubStyle(\ManiaLib\Gui\Elements\BgsPlayerCard::BgRacePlayerName);        
+        $this->scrollBg = new \ManiaLib\Gui\Elements\Quad(6, 80);
+        $this->scrollBg->setAlign("center", "top");
+        $this->scrollBg->setStyle("Bgs1");
+        $this->scrollBg->setSubStyle(\ManiaLib\Gui\Elements\Bgs1::BgCardSystem);
+        $this->addComponent($this->scrollBg);
+
+
+        $this->scrollUp = new \ManiaLib\Gui\Elements\Quad(8, 8);        
+        $this->scrollUp->setAlign("center", "top");
+        $this->scrollUp->setStyle("Icons64x64_1");
+        $this->scrollUp->setSubStyle(\ManiaLib\Gui\Elements\Icons64x64_1::ArrowUp);
+        $this->addComponent($this->scrollUp);
+
+        $this->scrollDown = new \ManiaLib\Gui\Elements\Quad(8, 8);        
+        $this->scrollDown->setAlign("center", "top");
+        $this->scrollDown->setStyle("Icons64x64_1");
+        $this->scrollDown->setSubStyle(\ManiaLib\Gui\Elements\Icons64x64_1::ArrowDown);
+        $this->addComponent($this->scrollDown);
+
+        $this->scroll = new \ManiaLib\Gui\Elements\Quad(5, 20);
+        $this->scroll->setAlign("center", "top");
+        $this->scroll->setStyle("Bgs1");
+        $this->scroll->setSubStyle(\ManiaLib\Gui\Elements\Bgs1::BgCard1);
         $this->scroll->setId("ScrollBar");
         $this->scroll->setScriptEvents();
         $this->addComponent($this->scroll);
@@ -30,7 +50,11 @@ class Pager extends \ManiaLive\Gui\Control {
 
     public function onResize($oldX, $oldY) {
         $this->pager->setSize($this->sizeX, $this->sizeY);
-        $this->scroll->setPosX($this->sizeX);
+         $this->scroll->setPosX($this->sizeX);
+        $this->scrollBg->setPosition($this->sizeX, 5.5);
+        $this->scrollBg->setSizeY($this->sizeY + 10);
+        $this->scrollUp->setPosition($this->sizeX, 6);
+        $this->scrollDown->setPosition($this->sizeX, -$this->sizeY+4);
     }
 
     public function setStretchContentX($value) {
@@ -96,7 +120,7 @@ EOD;
         if (moveScroll) {                                                                                                    
                     pagerDelta += MouseY - pagerMouseY;
                         
-                    declare max = (-6.0 * itemsPerPage)+40 ;
+                    declare max = (-6.0 * itemsPerPage)+20 ;
 
                     if (pagerDelta >= 0) {
                             pagerDelta = 0.0;                     
