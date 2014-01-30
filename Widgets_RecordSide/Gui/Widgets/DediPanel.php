ed0b58c610281ea67485f68a1e2a48cf7e352fa1
Petri Järvisalo <petri.jarvisalo@gmail.com>
2014-01-30 23:44:46 +0200

    fix

--- Widgets_RecordSide/Gui/Widgets/DediPanel.php
+++ Widgets_RecordSide/Gui/Widgets/DediPanel.php
@@ -60,8 +60,7 @@ class DediPanel extends LocalPanel {
 
     function fixHyphens($string) {
 	$out = str_replace('"', "'", $string);
-	$out = str_replace("\'", "'", $out);
-	$out = str_replace('\\', '\\\\', $string);
+	$out = str_replace('\\', '\\\\', $out);
 	
 	
 	return $out;
