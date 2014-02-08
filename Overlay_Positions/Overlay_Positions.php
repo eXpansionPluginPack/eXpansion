bf780f0e438b2eeea94333d922f34073a05ed037
Petri Järvisalo <petri.jarvisalo@gmail.com>
2014-02-08 19:17:39 +0200

    fix for laps mode

--- Overlay_Positions/Overlay_Positions.php
+++ Overlay_Positions/Overlay_Positions.php
@@ -17,120 +17,123 @@ class Overlay_Positions extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin
     private $wasWarmup = false;
 
     function exp_onInit() {
-        $this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_ROUNDS);
-        $this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_TEAM);
-        $this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_LAPS);
-        $this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_CUP);
+	$this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_ROUNDS);
+	$this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_TEAM);
+	$this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_LAPS);
+	$this->exp_addGameModeCompability(\Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_CUP);
     }
 
     public function exp_onReady() {
-        $this->enableDedicatedEvents();
-        $this->enableTickerEvent();
-        $this->update = true;
+	$this->enableDedicatedEvents();
+	$this->enableTickerEvent();
+	$this->update = true;
     }
 
     public function onTick() {
-        if ($this->update) {
-            $this->update = false;
-            if ($this->isPodium)
-                return;
+	if ($this->update) {
+	    $this->update = false;
+	    if ($this->isPodium)
+		return;
 
-            foreach ($this->retiredPlayers as $login => $player) {
-                $this->showWidget($login);
-            }
-            foreach ($this->storage->spectators as $login => $player) {
-                $this->showWidget($login);
-            }
-        }
+	    foreach ($this->retiredPlayers as $login => $player) {
+		$this->showWidget($login);
+	    }
+	    foreach ($this->storage->spectators as $login => $player) {
+		$this->showWidget($login);
+	    }
+	}
     }
 
     public function onEndMatch($rankings, $winnerTeamOrMap) {
-        if ($this->wasWarmup)
-            return;
+	if ($this->wasWarmup)
+	    return;
 
-        $this->isPodium = true;
-        $this->update = false;
-        $this->retiredPlayers = array();
-        Gui\Widgets\PositionPanel::EraseAll();
+	$this->isPodium = true;
+	$this->update = false;
+	$this->retiredPlayers = array();
+	Gui\Widgets\PositionPanel::EraseAll();
     }
 
     public function onBeginMap($map, $warmUp, $matchContinuation) {
-        $this->isPodium = false;
+	$this->isPodium = false;
     }
 
     public function hideWidget($login) {
-        Gui\Widgets\PositionPanel::Erase($login);
+	Gui\Widgets\PositionPanel::Erase($login);
     }
 
     public function showWidget($login) {
-        $pospanel = Gui\Widgets\PositionPanel::Create($login);
-        $pospanel->setSize(80, 90);
-        $pospanel->setPosition(-158, 40);
-        $pospanel->setData(\ManiaLivePlugins\eXpansion\Core\Core::$playerInfo, $this->storage->gameInfos->gameMode, $this->storage->gameInfos->teamMaxPoints);
-        $pospanel->setScale(1.1);
-        $pospanel->show();
+	$pospanel = Gui\Widgets\PositionPanel::Create($login);
+	$pospanel->setSize(80, 90);
+	$pospanel->setPosition(-158, 40);
+	$pospanel->setData(\ManiaLivePlugins\eXpansion\Core\Core::$playerInfo, $this->storage->gameInfos->gameMode, $this->storage->gameInfos->teamMaxPoints);
+	$pospanel->setScale(1.1);
+	$pospanel->show();
     }
 
     public function onPlayerInfoChanged($playerInfo) {
-        if ($this->storage->serverStatus->code != 4)
-            return;
+	if ($this->storage->serverStatus->code != 4)
+	    return;
 
-        $player = \Maniaplanet\DedicatedServer\Structures\Player::fromArray($playerInfo);
-        // hide widget for players who change from spectate to play
-        // on team mode, show infos when player is finished
+	$player = \Maniaplanet\DedicatedServer\Structures\Player::fromArray($playerInfo);
+	// hide widget for players who change from spectate to play
+	// on team mode, show infos when player is finished
 
-        if ($player->temporarySpectator == 1) {
-            $this->retiredPlayers[$player->login] = $this->storage->getPlayerObject($player->login);
-        } else {
-            if (array_key_exists($player->login, $this->retiredPlayers)) {
-                unset($this->retiredPlayers[$player->login]);
-            }
-        }
+	if ($player->temporarySpectator == 1) {
+	    $this->retiredPlayers[$player->login] = $this->storage->getPlayerObject($player->login);
+	} else {
+	    if (array_key_exists($player->login, $this->retiredPlayers)) {
+		unset($this->retiredPlayers[$player->login]);
+	    }
+	}
 
-        $this->update = true;
+	$this->update = true;
     }
 
     public function onBeginRound() {
-        $this->retiredPlayers = array();
-        $this->wasWarmup = $this->connection->getWarmUp();
+	$this->retiredPlayers = array();
+	$this->wasWarmup = $this->connection->getWarmUp();
     }
 
     public function onPlayerCheckpoint($playerUid, $login, $timeOrScore, $curLap, $checkpointIndex) {
-        $this->update = true;
+	$this->update = true;
     }
 
     public function onPlayerFinish($playerUid, $login, $timeOrScore) {
-        // on first thing when a round or match begins, onPlayerFinish is triggered for server login
-        // so hide the widget for players
-        if ($playerUid == 0) {
-            foreach ($this->retiredPlayers as $login => $player) {
-                $this->hideWidget($login);
-            }
-            $this->retiredPlayers = array();
+	// on first thing when a round or match begins, onPlayerFinish is triggered for server login
+	// so hide the widget for players
+	if ($playerUid == 0) {
+	    foreach ($this->retiredPlayers as $login => $player) {
+		$this->hideWidget($login);
+	    }
+	    $this->retiredPlayers = array();
 
-            foreach ($this->storage->players as $login => $player) {
-                $this->hideWidget($login);
-            }
-        }
-        // display widget to finished player
-        if ($timeOrScore > 0) {
-            $this->retiredPlayers[$login] = $this->storage->getPlayerObject($login);
-        }
-        $this->update = true;
+	    foreach ($this->storage->players as $login => $player) {
+		$this->hideWidget($login);
+	    }
+	}
+	
+	if (!$this->storage->currentMap->lapRace) {
+	    // display widget to finished player
+	    if ($timeOrScore > 0) {
+		$this->retiredPlayers[$login] = $this->storage->getPlayerObject($login);
+	    }
+	    $this->update = true;
+	}
     }
 
     public function onPlayerGiveup(ExpPlayer $player) {
-        $this->update = true;
-        $this->retiredPlayers[$player->login] = $player;
+	$this->update = true;
+	$this->retiredPlayers[$player->login] = $player;
     }
 
     public function onPlayerDisconnect($login, $disconnectionReason = null) {
-        $this->update = true;
+	$this->update = true;
     }
 
     public function onUnload() {
-        $this->disableTickerEvent();
-        parent::onUnload();
+	$this->disableTickerEvent();
+	parent::onUnload();
     }
 
 }
