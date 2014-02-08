a8443790ce0552ecdc65d5d21e5b44490f051f97
oliverde8 <oliverde8@gmail.com>
2014-02-08 09:50:18 +0100

    Clock widget can be moved again & Optimization remains

--- Widgets_Clock/Gui/Scripts_Clock/declarationScript.php
+++ Widgets_Clock/Gui/Scripts_Clock/declarationScript.php
@@ -2,3 +2,4 @@ declare CMlLabel lbl_clock <=> (Page.GetFirstChild("clock") as CMlLabel);
 declare playerLabel = (Page.GetFirstChild("nbPlayer") as CMlLabel);
 declare specLabel = (Page.GetFirstChild("nbSpec") as CMlLabel);
 declare serverLogin = "<?=$this->serverLogin?>";
+declare eXp_lastClockUpdate = 0;
--- Widgets_Clock/Gui/Scripts_Clock/whileLoopScript.php
+++ Widgets_Clock/Gui/Scripts_Clock/whileLoopScript.php
@@ -1,20 +1,22 @@
-lbl_clock.SetText(""^TextLib::SubString(CurrentLocalDateText, 11, 2)^":"^TextLib::SubString(CurrentLocalDateText, 14, 2)^":"^TextLib::SubString(CurrentLocalDateText, 17, 2));
 
-declare nbSpec = 0;
-declare nbPlayer = 0;
-foreach (Player in Players) {
-    if(Player.Login != serverLogin){
-        if(!Player.RequestsSpectate){
-            //log(Player.Login^"Is Player");
-            nbPlayer += 1;
-        }else{
-            //log(Player.Login^"Is Spec");
-            nbSpec += 1;
+if( (Now - eXp_lastClockUpdate) >= 1000 ){
+    lbl_clock.SetText(""^TextLib::SubString(CurrentLocalDateText, 11, 2)^":"^TextLib::SubString(CurrentLocalDateText, 14, 2)^":"^TextLib::SubString(CurrentLocalDateText, 17, 2));
+
+    declare nbSpec = 0;
+    declare nbPlayer = 0;
+    foreach (Player in Players) {
+        if(Player.Login != serverLogin){
+            if(!Player.RequestsSpectate){
+                //log(Player.Login^"Is Player");
+                nbPlayer += 1;
+            }else{
+                //log(Player.Login^"Is Spec");
+                nbSpec += 1;
+            }
         }
-    }
-} 
+    } 
 
-playerLabel.SetText(""^nbPlayer);
-specLabel.SetText(""^nbSpec);
-
-sleep(1000);
+    playerLabel.SetText(""^nbPlayer);
+    specLabel.SetText(""^nbSpec);
+    eXp_lastClockUpdate = Now;
+}
