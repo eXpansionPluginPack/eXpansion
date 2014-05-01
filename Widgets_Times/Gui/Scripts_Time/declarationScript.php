692634298de550ef6a0a35ef5524dcc14456aef2
oliverde8 <oliverde8@gmail.com>
2014-05-01 18:29:11 +0200

    Replaced Cp position better taking in account Lap as well

--- Widgets_Times/Gui/Scripts_Time/declarationScript.php
+++ Widgets_Times/Gui/Scripts_Time/declarationScript.php
@@ -5,4 +5,3 @@ declare Integer curCp = 0;
 declare Integer totalCp = <?php echo $this->totalCp ?>;
 declare Boolean lapRace = <?php echo $this->lapRace ?>;
 declare Text target = "<?php echo $this->target; ?>";
-log("Widget Refreshed");
