ba2fa070fcd2184cd8941563379ba4edb182eb97
oliverde8 <oliverde8@gmail.com>
2014-05-13 23:35:56 +0200

    Added new helpers

---
+++ Helpers/Paths.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace ManiaLivePlugins\eXpansion\Helpers;
+
+
+use ManiaLivePlugins\eXpansion\Core\Core;
+
+class Paths
+{
+
+    private $mapsDirectory;
+
+    function __construct()
+    {
+	$connection = Helper::getSingletons()->getDediConnection();
+	$this->mapsDirectory =  $connection->getMapsDirectory();
+    }
+
+
+    protected function getBaseMap(){
+	/**
+	 * @var \ManiaLivePlugins\eXpansion\Core\Config $config
+	 */
+	$config = \ManiaLivePlugins\eXpansion\Core\Config::getInstance();
+	return ($config->mapBase == "" ? "" : $config->mapBase.'/');
+    }
+
+    public function getMapPath()
+    {
+	return $this->mapsDirectory.'/'.$this->getBaseMap();
+    }
+
+    public function getDefaultMapPath(){
+	return $this->mapsDirectory.'/';
+    }
+
+    public function getMatchSettingPath(){
+	return $this->mapsDirectory.$this->getBaseMap().'MatchSettings/';
+    }
+
+    public function getDownloadMapsPath(){
+	return $this->mapsDirectory.$this->getBaseMap().'Downloaded/';
+    }
+} 
