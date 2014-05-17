12be398d87ab7f7b5b13c12a5db358a8e0285127
oliverde8 <oliverde8@gmail.com>
2014-05-17 15:26:02 +0200

    Added all git stuff

--- AutoUpdate/MetaData.php
+++ AutoUpdate/MetaData.php
@@ -3,6 +3,7 @@
 namespace ManiaLivePlugins\eXpansion\AutoUpdate;
 
 use ManiaLivePlugins\eXpansion\Core\types\config\types\Boolean;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\String;
 
 /**
  * Description of MetaData
@@ -17,7 +18,20 @@ class MetaData extends \ManiaLivePlugins\eXpansion\Core\types\config\MetaData {
 	$this->setDescription("Provides auto update service requests and ingame updates");
 
 	$config = Config::getInstance();
-	$var = new Boolean("autoCheckUpdates", "Auto check updates service for new update on administator connect ingame ?", $config);	
+	$var = new Boolean("autoCheckUpdates", "Auto check updates when administator connects ?", $config);
+	$var->setDescription("!! This won't work with git update !!");
+	$var->setDefaultValue(true);
+	$this->registerVariable($var);
+
+	$config = Config::getInstance();
+	$var = new Boolean("useGit", "Use git to update server", $config);
+	$var->setDescription("!! You need to have git installed for this to work !!");
+	$var->setDefaultValue(true);
+	$this->registerVariable($var);
+
+	$config = Config::getInstance();
+	$var = new String("branchName", "Name of the branch to update with", $config);
+	$var->setDescription("master : Stable, nighty : latest fixes not tested, dev : just might work :D");
 	$var->setDefaultValue(true);
 	$this->registerVariable($var);
     }
