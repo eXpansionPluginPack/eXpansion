b5059425a562acc07eb0ea2afc48e23fdc6eee3a
oliverde8 <oliverde8@gmail.com>
2014-05-26 22:10:48 +0200

    Cleaned TimeToText

--- Gui/Script_libraries/TimeToText.php
+++ Gui/Script_libraries/TimeToText.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace ManiaLivePlugins\eXpansion\Gui\Script_libraries;
+use ManiaLivePlugins\eXpansion\Gui\Structures\Script;
 
 /**
  * Description of ButtonScript
  *
  * @author De Cramer Oliver
  */
-class TimeToText extends \ManiaLivePlugins\eXpansion\Gui\Structures\Script{
+class TimeToText extends Script{
     
     function __construct() {
         parent::__construct("Gui/Script_libraries/TimeToText");
