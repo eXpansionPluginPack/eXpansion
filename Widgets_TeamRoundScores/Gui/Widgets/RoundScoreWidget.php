4836ff21d3e98e236062456d00a4138700a3a7fb
Petri Järvisalo <petri.jarvisalo@gmail.com>
2014-05-27 16:55:11 +0300

    limit widget to first 12 entries

--- Widgets_TeamRoundScores/Gui/Widgets/RoundScoreWidget.php
+++ Widgets_TeamRoundScores/Gui/Widgets/RoundScoreWidget.php
@@ -55,6 +55,8 @@ class RoundScoreWidget extends Widget {
 
     public function setScores($roundScores) {
 	foreach ($roundScores as $score) {
+	    if ($score->roundNumber > 12)
+		return;
 	    $this->frame->addComponent(new ScoreItem($score));
 	}
     }
