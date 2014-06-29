a98db9edb132a49f872ae9b4c7c03e7df7745265
Petri Järvisalo <petri.jarvisalo@gmail.com>
2014-06-29 10:27:26 +0300

    speed meter!

---
+++ Widgets_Speedometer/Gui/Widgets/Speedmeter.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * Copyright (C) 2014 Reaby
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace ManiaLivePlugins\eXpansion\Widgets_Speedometer\Gui\Widgets;
+
+/**
+ * Description of Speedmeter
+ *
+ * @author Reaby
+ */
+class Speedmeter extends \ManiaLivePlugins\eXpansion\Gui\Widgets\Widget
+{
+
+    function exp_onBeginConstruct()
+    {
+	$this->setName("Speed'o'meter");
+	
+	$label = new \ManiaLib\Gui\Elements\Label(20,6);
+	$label->setTextColor("fff");
+	$label->setTextSize(2);
+	$label->setId("speed");
+	$label->setText("n/a");
+	$label->setAlign("center", "top");
+	$label->setPosition(0,-6);
+	$this->addComponent($label);
+	
+	
+	$bg = new \ManiaLib\Gui\Elements\Quad(48,48);
+	$bg->setAlign("center", "center");
+	$bg->setImage("http://reaby.kapsi.fi/ml/speedbg.png", true);
+	$this->addComponent($bg);
+	
+	$needle = new \ManiaLib\Gui\Elements\Quad(48,48);
+	$needle->setAlign("center", "center");
+	$needle->setId("needle");
+	$needle->setImage("http://reaby.kapsi.fi/ml/speedneedle.png", true);
+	$this->addComponent($needle);
+	
+
+	$script = new \ManiaLivePlugins\eXpansion\Gui\Structures\Script("Widgets_Speedometer\Gui\Script");
+	$this->registerScript($script);
+    }
+
+}
---
+++ Widgets_Speedometer/MetaData.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ManiaLivePlugins\eXpansion\Widgets_Speedometer;
+
+use ManiaLivePlugins\eXpansion\Core\types\config\types\String;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\BasicList;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\Boolean;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\Int;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\BoundedInt;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\Float;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\BoundedFloat;
+
+/**
+ * Description of MetaData
+ *
+ * @author Petri
+ */
+class MetaData extends \ManiaLivePlugins\eXpansion\Core\types\config\MetaData {
+
+    public function onBeginLoad() {
+	parent::onBeginLoad();
+	$this->setName("Speedometer");
+	$this->setDescription("Provides speedometer");
+    }
+
+}
