554ebaae5eee2db6a5ab53f307375133336242ce
oliverde8 <oliverde8@gmail.com>
2014-07-20 10:39:36 +0200

    Added hook to ManiaExchange search to remove queu button as a test

--- MapSuggestion/MapSuggestion.php
+++ MapSuggestion/MapSuggestion.php
@@ -2,22 +2,42 @@
 
 namespace ManiaLivePlugins\eXpansion\MapSuggestion;
 
-use ManiaLivePlugins\eXpansion\AdminGroups\Permission;
+use ManiaLive\Event\Dispatcher;
+use ManiaLivePlugins\eXpansion\ManiaExchange\Hooks\ListButtons;
+use ManiaLivePlugins\eXpansion\ManiaExchange\Hooks\ListButtons_Event;
 
-class MapSuggestion extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin
+class MapSuggestion extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin implements ListButtons_Event
 {
 
-    public function exp_onReady()
-    {
-	$this->registerChatCommand("mapwish", "mapSuggestChatCommand", 0, true);
-    }
+	public function exp_onReady()
+	{
+		$this->registerChatCommand("mapwish", "mapSuggestChatCommand", 0, true);
 
-    public function mapSuggestChatCommand($login)
-    {
-	$window = Gui\Windows\MapWish::Create($login);
-	$window->show();
-    }
+		Dispatcher::register(ListButtons::getClass(), $this);
+	}
 
+
+
+	public function mapSuggestChatCommand($login)
+	{
+		$window = Gui\Windows\MapWish::Create($login);
+		$window->show();
+	}
+
+	/**
+	 *
+	 * @param $buttons
+	 * @param $login
+	 *
+	 * @return mixed
+	 */
+	public function hook_ManiaExchangeListButtons($buttons, $login)
+	{
+		if(isset($buttons->data['queue'])){
+			echo "Removing \n";
+			unset($buttons->data['queue']);
+		}
+	}
 }
 
 ?>
