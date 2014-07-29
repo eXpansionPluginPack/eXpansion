f0072cd73f7ef9cd15d17c40e9666ab47bee5d05
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-07-29 17:18:44 +0300

    fix maps widgets when restarting the map, fix bestruns widget, fix unloading best checkpoints, require very latest dedicated server

--- Widgets_BestCheckpoints/Widgets_BestCheckpoints.php
+++ Widgets_BestCheckpoints/Widgets_BestCheckpoints.php
@@ -8,71 +8,67 @@ use ManiaLivePlugins\eXpansion\Widgets_BestCheckpoints\Structures\Checkpoint;
 class Widgets_BestCheckpoints extends \ManiaLivePlugins\eXpansion\Core\types\ExpPlugin
 {
 
-    private $bestCps = array();
-
-    function exp_onLoad()
-    {
-	$this->enableDedicatedEvents();
-    }
-
-    public function exp_onReady()
-    {
-	$this->onBeginMatch();
-    }
-
-    /**
-     * displayWidget(string $login)
-     *
-     * @param string $login
-     */
-    function displayWidget($login = null)
-    {
-	$info = BestCpPanel::Create($login);
-	$info->setSize(190, 7);
-	$info->setPosition(-112, 90);
-	$info->setAlign("center", "top");
-	$info->show();
-    }
-
-    public function onBeginMatch()
-    {
-	$this->bestCps = new \SplFixedArray($this->storage->currentMap->nbCheckpoints);
-	for ($x = 0; $x < $this->storage->currentMap->nbCheckpoints; $x++) {
-	    $this->bestCps[$x] = new Checkpoint($x, "", "", 0);
+	private $bestCps = array();
+	
+	public function exp_onReady()
+	{
+		$this->enableDedicatedEvents();
+		$this->onBeginMatch();
 	}
-	BestCpPanel::$bestTimes = $this->bestCps;
 
-	$this->displayWidget(null);
-    }
+	/**
+	 * displayWidget(string $login)
+	 *
+	 * @param string $login
+	 */
+	function displayWidget($login = null)
+	{
+		$info = BestCpPanel::Create($login);
+		$info->setSize(190, 7);
+		$info->setPosition(-112, 90);
+		$info->setAlign("center", "top");
+		$info->show();
+	}
 
-    /* public function onPlayerCheckpoint($playerUid, $login, $timeOrScore, $curLap, $checkpointIndex) {
-      $checkpointIndex = $checkpointIndex % $this->storage->currentMap->nbCheckpoints;
+	public function onBeginMatch()
+	{
+		$this->bestCps = new \SplFixedArray($this->storage->currentMap->nbCheckpoints);
+		for ($x = 0; $x < $this->storage->currentMap->nbCheckpoints; $x++) {
+			$this->bestCps[$x] = new Checkpoint($x, "", "", 0);
+		}
+		BestCpPanel::$bestTimes = $this->bestCps;
+
+		$this->displayWidget(null);
+	}
+
+	/* public function onPlayerCheckpoint($playerUid, $login, $timeOrScore, $curLap, $checkpointIndex) {
+	  $checkpointIndex = $checkpointIndex % $this->storage->currentMap->nbCheckpoints;
 
 
-      // It only happens when multilap but fix on the top should fix this
-      // if (!isset($this->bestCps[$checkpointIndex]))
-      //     $this->bestCps[$checkpointIndex] = new Checkpoint($checkpointIndex, $this->storage->getPlayerObject($login)->nickName, $timeOrScore);
+	  // It only happens when multilap but fix on the top should fix this
+	  // if (!isset($this->bestCps[$checkpointIndex]))
+	  //     $this->bestCps[$checkpointIndex] = new Checkpoint($checkpointIndex, $this->storage->getPlayerObject($login)->nickName, $timeOrScore);
 
 
-      if ($this->bestCps[$checkpointIndex]->time > $timeOrScore || $this->bestCps[$checkpointIndex]->time == 0) {
-      $this->bestCps[$checkpointIndex] = new Checkpoint($checkpointIndex, $login, $this->storage->getPlayerObject($login)->nickName, $timeOrScore);
-      //BestCpPanel::RedrawAll();
-      }
-      } */
+	  if ($this->bestCps[$checkpointIndex]->time > $timeOrScore || $this->bestCps[$checkpointIndex]->time == 0) {
+	  $this->bestCps[$checkpointIndex] = new Checkpoint($checkpointIndex, $login, $this->storage->getPlayerObject($login)->nickName, $timeOrScore);
+	  //BestCpPanel::RedrawAll();
+	  }
+	  } */
 
-    public function onEndMatch($rankings, $winnerTeamOrMap)
-    {
-	BestCpPanel::EraseAll();
-	BestCpPanel::$bestTimes = array();
-	$this->bestCps = array();
-    }
+	public function onEndMatch($rankings, $winnerTeamOrMap)
+	{
+		BestCpPanel::EraseAll();
+		BestCpPanel::$bestTimes = array();
+		$this->bestCps = array();
+	}
 
-    function exp_onUnload()
-    {
-    	BestCpPanel::EraseAll();
-    }
+	function exp_onUnload()
+	{
+		BestCpPanel::EraseAll();
+		parent::exp_onUnload();
+	}
 
 }
-
 ?>
 
--- Widgets_BestRuns/Structures/Run.php
+++ Widgets_BestRuns/Structures/Run.php
@@ -2,19 +2,27 @@
 
 namespace ManiaLivePlugins\eXpansion\Widgets_BestRuns\Structures;
 
-class Run extends \Maniaplanet\DedicatedServer\Structures\AbstractStructure {
+use Maniaplanet\DedicatedServer\Structures\AbstractStructure;
+use Maniaplanet\DedicatedServer\Structures\PlayerRanking;
 
-    public $totalTime = 0;
-    public $nickname = "";
-    public $player;
-    public $checkpoints = array();
+class Run extends AbstractStructure
+{
 
-    function __construct(\ManiaLive\Data\Player $player) {
-	$this->player = $player;
-	$this->totalTime = $player->bestTime;
-	$this->nickname = $player->nickName;
-	$this->checkpoints = $player->bestCheckpoints;
-    }
+	public $totalTime = 0;
+
+	public $nickname = "";
+
+	public $player;
+
+	public $checkpoints = array();
+
+	function __construct(PlayerRanking $player)
+	{
+		$this->player = $player;
+		$this->totalTime = $player->bestTime;
+		$this->nickname = $player->nickName;
+		$this->checkpoints = $player->bestCheckpoints;
+	}
 
 }
 
