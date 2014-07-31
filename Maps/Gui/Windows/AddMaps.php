b7d9cfae8f43d8d99fbfdaf83c38534d5c5bfcc4
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-07-31 21:54:37 +0300

    fixes a problem with adding a local map + GUI for map adding window

--- Maps/Gui/Windows/AddMaps.php
+++ Maps/Gui/Windows/AddMaps.php
@@ -2,131 +2,146 @@
 
 namespace ManiaLivePlugins\eXpansion\Maps\Gui\Windows;
 
-
 use ManiaLivePlugins\eXpansion\Helpers\Helper;
 use \ManiaLivePlugins\eXpansion\Maps\Gui\Controls\Additem;
 
-class AddMaps extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window {
+class AddMaps extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
+{
 
-    private $pager;
-    public static $mapsPlugin = null;
+	private $pager;
 
-    /** @var  \Maniaplanet\DedicatedServer\Connection */
-    private $connection;
+	public static $mapsPlugin = null;
 
-    /** @var  \ManiaLive\Data\Storage */
-    private $storage;
-    private $items = array();
-    private $gbx;
-    private $btnAddAll;
-    private $actionAddAll;
-    private $label;
+	/** @var  \Maniaplanet\DedicatedServer\Connection */
+	private $connection;
 
-    protected function onConstruct() {
-        parent::onConstruct();
-        $config = \ManiaLive\DedicatedApi\Config::getInstance();
-        $this->connection = \Maniaplanet\DedicatedServer\Connection::factory($config->host, $config->port);
-        $this->storage = \ManiaLive\Data\Storage::getInstance();
-        $this->gbx = new \ManiaLivePlugins\eXpansion\Helpers\GBXChallMapFetcher(true, false, false);
-        $this->pager = new \ManiaLivePlugins\eXpansion\Gui\Elements\Pager();
-        $this->mainFrame->addComponent($this->pager);
+	/** @var  \ManiaLive\Data\Storage */
+	private $storage;
 
-        $this->actionAddAll = $this->createAction(array($this, "addAllMaps"));
+	private $items = array();
 
-        $this->btnAddAll = new \ManiaLivePlugins\eXpansion\Gui\Elements\Button();
-        $this->btnAddAll->setText(__("Add all", $this->getRecipient()));
-        $this->btnAddAll->setAction($this->actionAddAll);
-        $this->mainFrame->addComponent($this->btnAddAll);
-    }
+	private $gbx;
 
-    function addMap($login, $filename) {
-        try {
-            $this->connection->addMap($filename);
-            $mapinfo = $this->connection->getMapInfo($filename);
-            $this->connection->chatSendServerMessage(__('Map %s $z$s$fffadded to playlist.', $this->getRecipient(), $mapinfo->name));
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__('Error:', $e->getMessage()));
-        }
-    }
+	private $btnAddAll;
 
-    function onResize($oldX, $oldY) {
-        parent::onResize($oldX, $oldY);
-        $this->pager->setSize($this->sizeX, $this->sizeY - 12);
-        $this->pager->setStretchContentX($this->sizeX);
+	private $actionAddAll;
 
-        $this->btnAddAll->setPosition(4, -$this->sizeY + 6);
-    }
+	private $label;
 
-    function onShow() {
-        $this->populateList();
-    }
+	protected function onConstruct()
+	{
+		parent::onConstruct();
+		$config = \ManiaLive\DedicatedApi\Config::getInstance();
+		$this->connection = \Maniaplanet\DedicatedServer\Connection::factory($config->host, $config->port);
+		$this->storage = \ManiaLive\Data\Storage::getInstance();
+		$this->gbx = new \ManiaLivePlugins\eXpansion\Helpers\GBXChallMapFetcher(true, false, false);
+		$this->pager = new \ManiaLivePlugins\eXpansion\Gui\Elements\Pager();
+		$this->mainFrame->addComponent($this->pager);
 
-    function populateList() {
-        foreach ($this->items as $item) {
-           $item->erase();
-        }
+		$this->actionAddAll = $this->createAction(array($this, "addAllMaps"));
 
-        $this->pager->clearItems();
-        $this->items = array();
+		$this->btnAddAll = new \ManiaLivePlugins\eXpansion\Gui\Elements\Button();
+		$this->btnAddAll->setText(__("Add all", $this->getRecipient()));
+		$this->btnAddAll->setAction($this->actionAddAll);
+		$this->mainFrame->addComponent($this->btnAddAll);
+	}
 
-        $login = $this->getRecipient();
+	function addMap($login, $filename)
+	{
+		try {
+			$this->connection->addMap($filename);
+			$mapinfo = $this->connection->getMapInfo($filename);
+			$this->connection->chatSendServerMessage(__('Map %s $z$s$fffadded to playlist.', $this->getRecipient(), $mapinfo->name));
+		} catch (\Exception $e) {
+			$this->connection->chatSendServerMessage(__('Error:', $e->getMessage()));
+		}
+	}
 
-        /** @var \Maniaplanet\DedicatedServer\Structures\Version */
-        $game = $this->connection->getVersion();
-        $path = Helper::getPaths()->getDownloadMapsPath() . $game->titleId . "/*.Map.Gbx";
+	function onResize($oldX, $oldY)
+	{
+		foreach ($this->items as $item) {
+			$item->setSize($this->sizeX, $this->sizeY);
+		}
+		$this->pager->setSize($this->sizeX - 6, $this->sizeY - 12);
+		$this->btnAddAll->setPosition(4, -$this->sizeY + 6);
+		parent::onResize($oldX, $oldY);
+	}
 
-        $maps = glob($path);
-        $x = 0;
-        if (count($maps) >= 1) {
-            foreach ($maps as $map) {
-                $this->items[$x] = new Additem($x, $map, $this, $this->gbx, $login, $this->sizeX);
-                $this->pager->addItem($this->items[$x]);
-                $x++;
-            }
-        }
-    }
+	function onShow()
+	{
+		$this->populateList();
+	}
 
-    function deleteMap($login, $filename) {
-        try {
-            unlink($filename);
-            $file = explode("/", $filename);
-            $this->connection->chatSendServerMessage(__("File '%s' deleted from filesystem!", $this->getRecipient(), end($file)));
-            $this->populateList();
-            $this->RedrawAll();
-        } catch (\Exception $e) {
-            $this->connection->chatSendServerMessage(__('$f00$oError $z$s$fff%s', $this->getRecipient(), $e->getMessage()));
-        }
-    }
+	function populateList()
+	{
+		foreach ($this->items as $item) {
+			$item->erase();
+		}
 
-    function addAllMaps($login) {
-        $game = $this->connection->getVersion();
-        $path = Helper::getPaths()->getDownloadMapsPath() . $game->titleId . "/*.Map.Gbx";
+		$this->pager->clearItems();
+		$this->items = array();
 
-        $mapsAtDisk = glob($path);
+		$login = $this->getRecipient();
+
+		/** @var \Maniaplanet\DedicatedServer\Structures\Version */
+		$game = $this->connection->getVersion();
+		$path = Helper::getPaths()->getDownloadMapsPath() . $game->titleId . "/*.Map.Gbx";
+
+		$maps = glob($path);
+		$x = 0;
+		if (count($maps) >= 1) {
+			foreach ($maps as $map) {
+				$this->items[$x] = new Additem($x, $map, $this, $this->gbx, $login, $this->sizeX);
+				$this->pager->addItem($this->items[$x]);
+				$x++;
+			}
+		}
+	}
+
+	function deleteMap($login, $filename)
+	{
+		try {
+			unlink($filename);
+			$file = explode("/", $filename);
+			$this->connection->chatSendServerMessage(__("File '%s' deleted from filesystem!", $this->getRecipient(), end($file)));
+			$this->populateList();
+			$this->RedrawAll();
+		} catch (\Exception $e) {
+			$this->connection->chatSendServerMessage(__('$f00$oError $z$s$fff%s', $this->getRecipient(), $e->getMessage()));
+		}
+	}
+
+	function addAllMaps($login)
+	{
+		$game = $this->connection->getVersion();
+		$path = Helper::getPaths()->getDownloadMapsPath() . $game->titleId . "/*.Map.Gbx";
+
+		$mapsAtDisk = glob($path);
 //        $mapsAtServer = array();
 //        foreach ($this->storage->maps as $map) {
 //            $mapsAtServer[] = str_replace("\\", "/", $mapsDir . $map->fileName);
 //        }
 //        $mapDiff = array_diff($mapsAtServer, $mapsAtDisk);
 
-        $this->connection->addMapList($mapsAtDisk);
-        $this->connection->chatSendServerMessage("Added " . count($mapsAtDisk) . " maps to playlist.", $login);
-    }
+		$this->connection->addMapList($mapsAtDisk);
+		$this->connection->chatSendServerMessage("Added " . count($mapsAtDisk) . " maps to playlist.", $login);
+	}
 
-    function destroy() {
-        $this->gbx = null;
-        foreach ($this->items as $item) {
-           $item->erase();
-        }
-        $this->items = array();
-        $this->btnAddAll->destroy();
-        $this->connection = null;
-        $this->storage = null;
-        $this->pager->destroy();
-        $this->clearComponents();
+	function destroy()
+	{
+		$this->gbx = null;
+		foreach ($this->items as $item) {
+			$item->erase();
+		}
+		$this->items = array();
+		$this->btnAddAll->destroy();
+		$this->connection = null;
+		$this->storage = null;
+		$this->pager->destroy();
+		$this->clearComponents();
 
-        parent::destroy();
-    }
+		parent::destroy();
+	}
 
 }
 
