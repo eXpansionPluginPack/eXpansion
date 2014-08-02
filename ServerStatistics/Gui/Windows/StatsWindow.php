959c8f7a316a5500916120895dfe099491794334
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-08-02 21:15:53 +0300

    fixes

--- ServerStatistics/Gui/Windows/StatsWindow.php
+++ ServerStatistics/Gui/Windows/StatsWindow.php
@@ -70,7 +70,7 @@ class StatsWindow extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window {
         $this->contentFrame->setScale(.8);
         $this->mainFrame->addComponent($this->contentFrame);
         
-        $this->contentFrame->addComponent(new InfoLine(25, 'Comment', $storage->server->comment,0));
+        $this->contentFrame->addComponent(new InfoLine(25, 'Comment', \ManiaLivePlugins\eXpansion\Gui\Gui::fixString($storage->server->comment),0));
         $this->contentFrame->addComponent(new InfoLine(5, 'Up Time', $data['upTime'],0));
         $this->contentFrame->addComponent(new InfoLine(5, 'Map Count', sizeof($storage->maps),0));
         
