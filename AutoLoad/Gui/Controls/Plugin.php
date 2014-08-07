7d4dd806110cc6e6355065e3bb4db59f8b102aa2
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-08-07 21:15:45 +0300

    way better plugins manager UI

--- AutoLoad/Gui/Controls/Plugin.php
+++ AutoLoad/Gui/Controls/Plugin.php
@@ -68,6 +68,7 @@ class Plugin extends \ManiaLive\Gui\Control
 
 		$this->metaData = $plugin;
 		$this->autoLoad = $autoload;
+		$toggleAction = $this->createAction(array($this, "togglePlugin"));
 		
 		$this->bg = new ListBackGround($indexNumber, 100, 4);
 		$this->addComponent($this->bg);
@@ -81,37 +82,24 @@ class Plugin extends \ManiaLive\Gui\Control
 		$this->button_running = new Button(8, 8);
 		$this->button_running->setDescription(__($this->getRunningDescriptionText($isLoaded, $isInStart), $login), 120);
 		if ($isLoaded)
-			$this->button_running->setIcon($guiConfig->getImage("statusbuttons", "3_off.png"));
+			$this->button_running->setIcon($guiConfig->getImage("statusbuttons", "3_on.png"));
 		else if ($isInStart)
 			$this->button_running->setIcon($guiConfig->getImage("statusbuttons", "2_on.png"));
 		else
-			$this->button_running->setIcon($guiConfig->getImage("statusbuttons", "1_on.png"));
+			$this->button_running->setIcon($guiConfig->getImage("statusbuttons", "1_off.png"));
+		$this->button_running->setAction($toggleAction);
 		$this->addComponent($this->button_running);
-
-		$this->icon_name = new Quad();
-		$this->icon_name->setSize(4, 4);
-		$this->icon_name->setStyle('Icons128x128_1');
-		$this->icon_name->setSubStyle('CustomStars');
-		$this->icon_name->setPosition(6, 4);
-		$this->addComponent($this->icon_name);
 
 		$this->label_name = new Label(40, 4);
 		$this->label_name->setScale(0.8);
 		$this->label_name->setText($plugin->getName() == "" ? $plugin->getPlugin() : $plugin->getName());
-		$this->label_name->setPosition(11, 3);
+		$this->label_name->setPosition(8, 3);
 		$this->addComponent($this->label_name);
-
-		$this->icon_author = new Quad();
-		$this->icon_author->setSize(4, 4);
-		$this->icon_author->setStyle('Icons64x64_1');
-		$this->icon_author->setSubStyle('IconPlayers');
-		$this->icon_author->setPosition(6, 0);
-		$this->addComponent($this->icon_author);
 
 		$this->label_author = new Label(40, 4);
 		$this->label_author->setScale(0.6);
 		$this->label_author->setText($plugin->getAuthor());
-		$this->label_author->setPosition(11, -1);
+		$this->label_author->setPosition(8, -1);
 		$this->addComponent($this->label_author);
 
 		$this->button_titleComp = new Button(7, 7);
@@ -143,7 +131,7 @@ class Plugin extends \ManiaLive\Gui\Control
 		//$this->addComponent($this->button_more);
 
 		$this->button_start = new Button(12, 5);
-		$this->button_start->setAction($this->createAction(array($this, "startPlugin")));
+		$this->button_start->setAction($toggleAction);
 		$this->button_start->setText(__($this->getStartText($isLoaded, $isInStart), $login));
 
 		if ($this->getStartText($isLoaded, $isInStart) == "Start") {
@@ -151,7 +139,7 @@ class Plugin extends \ManiaLive\Gui\Control
 		} else {
 			$this->button_start->colorize("F00");
 		}
-		$this->addComponent($this->button_start);
+	//	$this->addComponent($this->button_start);
 
 		$this->setSize(117, 8);
 	}
@@ -173,11 +161,11 @@ class Plugin extends \ManiaLive\Gui\Control
 	private function getRunningDescriptionText($runnig, $inStart)
 	{
 		if ($runnig)
-			return "This plugin is running on the server at the moment";
+			return "Plugin is running. Click to unload!";
 		else if ($inStart)
-			return "This plugin isn't compatible with game mode or title. It is therefor pending";
+			return "Plugin not compatible with game mode, title or server settings.\n Plugin will be enabled when possible.";
 		else
-			return "This plugin isn't running";
+			return "Plugin not running. Click to load!";
 	}
 
 	private function getTitleDescriptionText($titleCompatible)
@@ -213,7 +201,7 @@ class Plugin extends \ManiaLive\Gui\Control
 		}
 	}
 
-	public function startPlugin($login)
+	public function togglePlugin($login)
 	{
 		$this->autoLoad->tooglePlugin($login, $this->metaData);
 	}
