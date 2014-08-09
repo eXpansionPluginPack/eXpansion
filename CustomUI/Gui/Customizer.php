b82d06cabcba99451315cb984b41f0cda8037178
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-08-09 19:29:49 +0300

    experimental clientui test

--- CustomUI/Gui/Customizer.php
+++ CustomUI/Gui/Customizer.php
@@ -2,8 +2,10 @@
 
 namespace ManiaLivePlugins\eXpansion\CustomUI\Gui;
 
+use ManiaLib\Gui\Elements\Label;
 use ManiaLivePlugins\eXpansion\Gui\Structures\Script;
 use ManiaLivePlugins\eXpansion\Gui\Widgets\PlainWidget;
+
 
 class Customizer extends PlainWidget
 {
