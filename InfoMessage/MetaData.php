713129a2a8316002f474340247fb29b93064b339
Petri Jarvisalo <petri.jarvisalo@mbnet.fi>
2014-12-07 06:54:12 +0200

    Added Info Messages plugin

---
+++ InfoMessage/MetaData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ManiaLivePlugins\eXpansion\InfoMessage;
+
+use ManiaLivePlugins\eXpansion\Core\types\config\types\BasicList;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\Boolean;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\ColorCode;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\ConfigFile;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\Int;
+use ManiaLivePlugins\eXpansion\Core\types\config\types\String;
+
+
+/**
+ * Description of MetaData
+ *
+ * @author De Cramer Oliver
+ */
+class MetaData extends \ManiaLivePlugins\eXpansion\Core\types\config\MetaData
+{
+
+	public function onBeginLoad()
+	{
+		parent::onBeginLoad();
+		$this->setName("Info Messages");
+		$this->setDescription("Send informatic chat message every now and then");
+
+		$config = Config::getInstance();
+
+		$var = new BasicList("infoMessages", "Messages", $config, false,false);
+		$var->setType(new String(""));
+		$var->setDefaultValue(array());
+		$this->registerVariable($var);
+
+		$var = new String("infoInterval", "Interval in mm:ss", $config, false,false);
+		$var->setDefaultValue("1:00");
+		$this->registerVariable($var);
+
+	}
+
+}
+
+?>
