a7f6dc02aa1be1fb108056a6285b0cdad7bf2a40
reaby <petri.jarvisalo@gmail.com>
2015-11-11 16:15:38 +0200

    fix for #169 and added adminpanel permission check for balance teams

--- Menu/Gui/Controls/PanelItem.php
+++ Menu/Gui/Controls/PanelItem.php
@@ -7,89 +7,98 @@ use ManiaLivePlugins\eXpansion\Gui\Config;
 
 class PanelItem extends \ManiaLivePlugins\eXpansion\Gui\Control
 {
+    /** @var \ManiaLib\Gui\Elements\Quad */
+    protected $bg;
+    protected $nick;
+    protected $label;
+    protected $icon = null;
+    protected $time;
+    protected $frame;
 
-	/** @var \ManiaLib\Gui\Elements\Quad */
-	protected $bg;
+    function __construct($addArrow = false)
+    {
+        $config = Config::getInstance();
 
-	protected $nick;
+        $sizeX = 29;
+        $sizeY = 5.5;
+        $this->setAlign("left", "top");
 
-	protected $label;
+        $this->bg = new \ManiaLib\Gui\Elements\Quad($sizeX, $sizeY);
+        $this->bg->setAlign("left", "top");
+        $this->bg->setBgcolor($config->windowBackgroundColor);
+        $this->bg->setBgcolorFocus($config->style_widget_title_bgColorize);
+        $this->bg->setOpacity(0.75);
+        $this->bg->setScriptEvents();
+        $this->addComponent($this->bg);
 
-	protected $time;
+        $this->label = new \ManiaLib\Gui\Elements\Label($sizeX, $sizeY);
+        $this->label->setStyle("TextCardScores2");
+        $this->label->setTextSize(1);
+        $this->label->setPosX(3);
+        $this->label->setPosY(-$sizeY / 2);
+        $this->label->setAlign("left", "center");
+        $this->label->setTextEmboss();
+        $this->addComponent($this->label);
 
-	protected $frame;
+        if ($addArrow) {
+            $this->icon = new \ManiaLib\Gui\Elements\Quad(5.5, 5.5);
+            $this->icon->setStyle("Icons64x64_1");
+            $this->icon->setSubStyle("ShowRight2");
+            $this->icon->setPosX(25);
+            $this->addComponent($this->icon);
+        }
 
-	function __construct()
-	{
-		$config = Config::getInstance();
+        $this->setSize($sizeX, $sizeY);
+    }
 
-		$sizeX = 29;
-		$sizeY = 5.5;
-		$this->setAlign("left", "top");
+    function setText($text)
+    {
+        $this->label->setText($text);
+    }
 
-		$this->bg = new \ManiaLib\Gui\Elements\Quad($sizeX, $sizeY);
-		$this->bg->setAlign("left", "top");         
-		$this->bg->setBgcolor($config->windowBackgroundColor);
-                $this->bg->setBgcolorFocus($config->style_widget_title_bgColorize);
-		$this->bg->setOpacity(0.75);
-		$this->bg->setScriptEvents();
-		$this->addComponent($this->bg);
+    function setClass($value)
+    {
+        $this->bg->setAttribute("class", $value);
+        $this->label->setAttribute("class", $value."_lbl");
+        if ($this->icon !== null) {
+            $this->icon->setAttribute("class", $value."_icon");
+        }
+    }
 
-		$this->label = new \ManiaLib\Gui\Elements\Label($sizeX, $sizeY);
-		$this->label->setStyle("TextCardScores2");
-		$this->label->setTextSize(1);
-		$this->label->setPosX($sizeX / 2);
-		$this->label->setPosY(-$sizeY / 2);
-		$this->label->setAlign("center", "center");
-		$this->label->setTextEmboss();
-		$this->addComponent($this->label);
+    function setId($id)
+    {
+        $this->bg->setId($id);
+        if ($this->icon !== null) {
+            $this->icon->setId($id."_icon");
+        }
+        $this->label->setId($id."_lbl");
+    }
 
-		$this->setSize($sizeX, $sizeY);
-	}
+    function setAction($action)
+    {
+        $this->bg->setAttribute('data-action', $action);
+    }
 
-	function setText($text)
-	{
-		$this->label->setText($text);
-	}
+    function onIsRemoved(\ManiaLive\Gui\Container $target)
+    {
+        parent::onIsRemoved($target);
+        $this->destroy();
+    }
 
-	function setClass($value)
-	{
-		$this->bg->setAttribute("class", $value);
-		$this->label->setAttribute("class", $value . "_lbl");
-	}
+    function destroy()
+    {
+        parent::destroy();
+    }
 
-	function setId($id)
-	{
-		$this->bg->setId($id);
-		$this->label->setId($id . "_lbl");
-	}
-
-	function setAction($action)
-	{
-		$this->bg->setAttribute('data-action', $action);
-	}
-
-	function onIsRemoved(\ManiaLive\Gui\Container $target)
-	{
-		parent::onIsRemoved($target);
-		$this->destroy();
-	}
-
-	function destroy()
-	{
-		parent::destroy();
-	}
-
-	function setTop()
-	{
+    function setTop()
+    {
         // deprecated
-	}
+    }
 
-	function setBottom()
-	{
-	// deprecated
-	}
-
+    function setBottom()
+    {
+        // deprecated
+    }
 }
 ?>
 
--- Menu/Gui/Widgets/Submenu.php
+++ Menu/Gui/Widgets/Submenu.php
@@ -4,147 +4,134 @@ namespace ManiaLivePlugins\eXpansion\Menu\Gui\Widgets;
 
 class Submenu extends \ManiaLivePlugins\eXpansion\Gui\Widgets\PlainWidget
 {
+    private $menu, $debug, $bg;
+    public $myscript;
+    private $item = array();
+    private $submenu = array();
+    private $bgs = array();
+    private $storage;
 
-	private $menu, $debug, $bg;
+    public function addItem(&$menu, $text, $action = null, $submenuNb = false, $addArrow = false)
+    {
+        $nb              = count($this->item);
+        $this->item[$nb] = new \ManiaLivePlugins\eXpansion\Menu\Gui\Controls\PanelItem($addArrow);
 
-	public $myscript;
+        if (!empty($action)) {
+            $this->item[$nb]->setAction($action);
+        }
 
-	private $item = array();
+        $this->item[$nb]->setText("  ".$text);
 
-	private $submenu = array();
+        if ($submenuNb !== false) {
+            $this->item[$nb]->setId("sub_".$submenuNb);
+            $this->item[$nb]->setClass("menuitem");
+            // $this->item[$nb]->setFocusAreaColor2("3afb");
+        } else {
 
-	private $bgs = array();
+            $snb = false;
+            foreach ($this->submenu as $subNb => $sub) {
+                if ($sub === $menu) {
+                    $snb = $subNb;
+                    break;
+                }
+            }
+            if ($snb) {
+                $this->item[$nb]->setId("sub_".$snb."_item_".$nb);
+                $this->item[$nb]->setAction($action);
+                $this->item[$nb]->setClass("subitem");
+            } else {
+                $this->item[$nb]->setId("item_".$nb);
+                $this->item[$nb]->setClass("menuitem");
+            }
+        }
 
-	private $storage;
+        $menu->addComponent($this->item[$nb]);
+    }
 
-	public function addItem(&$menu, $text, $action = null, $submenuNb = false)
-	{
-		$nb = count($this->item);
-		$this->item[$nb] = new \ManiaLivePlugins\eXpansion\Menu\Gui\Controls\PanelItem();
+    public function addSubMenu(&$menu, $text, $action = null)
+    {
+        $mb                 = count($this->submenu) + 1;
+        $this->submenu[$mb] = new \ManiaLive\Gui\Controls\Frame(29.5, 5.5);
+        $this->submenu[$mb]->setLayout(new \ManiaLib\Gui\Layouts\Column());
+        $this->submenu[$mb]->setId("submenu_".$mb);
+        $this->submenu[$mb]->setScriptEvents();
+        // add item to menu
+        // $this->addItem($menu, $text . " » ", $action, $mb);
+        $this->addItem($menu, $text, $action, $mb, true);
+        // add component to menu
+        $menu->addComponent($this->submenu[$mb]);
+        return $this->submenu[$mb];
+    }
 
-		if (!empty($action)) {
-			$this->item[$nb]->setAction($action);
-		}
+    public function getMenu()
+    {
+        return $this->menu;
+    }
 
-		$this->item[$nb]->setText("  " . $text);
+    protected function onConstruct()
+    {
+        parent::onConstruct();
+        $this->storage = \ManiaLive\Data\Storage::getInstance();
+        $this->menu    = new \ManiaLive\Gui\Controls\Frame();
+        $this->menu->setLayout(new \ManiaLib\Gui\Layouts\Column());
+        $this->menu->setId("Submenu");
+        $this->menu->setScriptEvents();
+        $this->addComponent($this->menu);
 
-		if ($submenuNb !== false) {
-			$this->item[$nb]->setId("sub_" . $submenuNb);
-			$this->item[$nb]->setClass("menuitem");
-			// $this->item[$nb]->setFocusAreaColor2("3afb");
-		}
-		else {
+        $inputbox = new \ManiaLivePlugins\eXpansion\Gui\Elements\Inputbox("widgetStatus");
+        $inputbox->setPosition(900, 900);
+        $inputbox->setScriptEvents();
+        $this->addComponent($inputbox);
 
-			$snb = false;
-			foreach ($this->submenu as $subNb => $sub) {
-				if ($sub === $menu) {
-					$snb = $subNb;
-					break;
-				}
-			}
-			if ($snb) {
-				$this->item[$nb]->setId("sub_" . $snb . "_item_" . $nb);
-				$this->item[$nb]->setAction($action);
-				$this->item[$nb]->setClass("subitem");
-			}
-			else {
-				$this->item[$nb]->setId("item_" . $nb);
-				$this->item[$nb]->setClass("menuitem");
-			}
-		}
+        $lib = new \ManiaLivePlugins\eXpansion\Gui\Script_libraries\Animation();
+        $this->registerScript($lib);
 
-		$menu->addComponent($this->item[$nb]);
-	}
+        $this->myscript = new \ManiaLivePlugins\eXpansion\Gui\Structures\Script("Menu\Gui\Scripts");
+        $this->registerScript($this->myscript);
+    }
 
-	public function addSubMenu(&$menu, $text, $action = null)
-	{
-		$mb = count($this->submenu) + 1;
-		$this->submenu[$mb] = new \ManiaLive\Gui\Controls\Frame(29.5, 5.5);
-		$this->submenu[$mb]->setLayout(new \ManiaLib\Gui\Layouts\Column());
-		$this->submenu[$mb]->setId("submenu_" . $mb);
-		$this->submenu[$mb]->setScriptEvents();
-		// add item to menu
-		// $this->addItem($menu, $text . " » ", $action, $mb);
-		$this->addItem($menu, $text, $action, $mb);
-		// add component to menu
-		$menu->addComponent($this->submenu[$mb]);
+    protected function onDraw()
+    {
+        $storage = \ManiaLive\Data\Storage::getInstance();
 
-		return $this->submenu[$mb];
-	}
+        $this->item[0]->setTop();
+        $this->item[count($this->item) - 1]->setBottom();
 
-	public function getMenu()
-	{
-		return $this->menu;
-	}
+        foreach ($this->submenu as &$item) {
+            $comp = $item->getComponents();
+            if (empty($comp)) continue;
+            reset($comp);
+            current($comp)->setTop();
+            end($comp)->setBottom();
+        }
 
-	protected function onConstruct()
-	{
-		parent::onConstruct();
-		$this->storage = \ManiaLive\Data\Storage::getInstance();
-		$this->menu = new \ManiaLive\Gui\Controls\Frame();
-		$this->menu->setLayout(new \ManiaLib\Gui\Layouts\Column());
-		$this->menu->setId("Submenu");
-		$this->menu->setScriptEvents();
-		$this->addComponent($this->menu);
+        $count   = count($this->submenu);
+        $version = \ManiaLivePlugins\eXpansion\Core\Core::EXP_VERSION;
+        $this->myscript->setParam("version", $version);
+        $this->myscript->setParam("name", "Submenu");
 
-		$inputbox = new \ManiaLivePlugins\eXpansion\Gui\Elements\Inputbox("widgetStatus");
-		$inputbox->setPosition(900, 900);
-		$inputbox->setScriptEvents();
-		$this->addComponent($inputbox);
+        if ($this->storage->gameInfos->gameMode == \Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_SCRIPT) {
+            $this->myscript->setParam("gameMode", \ManiaLivePlugins\eXpansion\Gui\Gui::fixString($this->storage->gameInfos->scriptName));
+        } else {
+            $this->myscript->setParam("gameMode", $this->storage->gameInfos->gameMode);
+        }
+        $this->myscript->setParam("forceReset", $this->getBoolean(DEBUG));
+        $this->myscript->setParam("count", $count);
 
-		$lib = new \ManiaLivePlugins\eXpansion\Gui\Script_libraries\Animation();
-		$this->registerScript($lib);
+        parent::onDraw();
+    }
 
-		$this->myscript = new \ManiaLivePlugins\eXpansion\Gui\Structures\Script("Menu\Gui\Scripts");
-		$this->registerScript($this->myscript);
-	}
+    protected function onResize($oldX, $oldY)
+    {
+        parent::onResize($oldX, $oldY);
+    }
 
-	protected function onDraw()
-	{
-		$storage = \ManiaLive\Data\Storage::getInstance();
-
-		$this->item[0]->setTop();
-		$this->item[count($this->item) - 1]->setBottom(); 
-
-		foreach ($this->submenu as &$item) {
-			$comp = $item->getComponents();
-			if (empty($comp))
-				continue;
-			reset($comp);
-			current($comp)->setTop();
-			end($comp)->setBottom(); 
-		}
-
-		$count = count($this->submenu);
-		$version = \ManiaLivePlugins\eXpansion\Core\Core::EXP_VERSION;
-		$this->myscript->setParam("version", $version);
-		$this->myscript->setParam("name", "Submenu");
-
-		if ($this->storage->gameInfos->gameMode == \Maniaplanet\DedicatedServer\Structures\GameInfos::GAMEMODE_SCRIPT) {
-			$this->myscript->setParam("gameMode", \ManiaLivePlugins\eXpansion\Gui\Gui::fixString($this->storage->gameInfos->scriptName));
-		}
-		else {
-			$this->myscript->setParam("gameMode", $this->storage->gameInfos->gameMode);
-		}
-		$this->myscript->setParam("forceReset", $this->getBoolean(DEBUG));
-		$this->myscript->setParam("count", $count);
-
-		parent::onDraw();
-	}
-
-	protected function onResize($oldX, $oldY)
-	{
-		parent::onResize($oldX, $oldY);
-	}
-
-	public function destroy()
-	{
-		unset($this->item);
-		unset($this->submenu);
-		unset($this->bgs);
-		parent::destroy();
-	}
-
+    public function destroy()
+    {
+        unset($this->item);
+        unset($this->submenu);
+        unset($this->bgs);
+        parent::destroy();
+    }
 }
-
 ?>
