0157d4848176b22538708b3dc70816216bf0405f
reaby <petri.jarvisalo@gmail.com>
2015-11-12 10:12:03 +0200

    dedimania enhancements: new column: login and a button to reporting for invalid dedimania times

---
+++ Dedimania/Gui/Windows/DediReport.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace ManiaLivePlugins\eXpansion\Dedimania\Gui\Windows;
+
+use ManiaLib\Gui\Elements\Frame;
+use ManiaLib\Gui\Elements\Label;
+use ManiaLib\Gui\Layouts\Column;
+use ManiaLive\Data\Storage;
+use ManiaLivePlugins\eXpansion\Gui\Elements\TextEdit;
+use ManiaLivePlugins\eXpansion\Gui\Windows\Window;
+
+/**
+ * Description of DediReport
+ *
+ * @author Petri Järvisalo <petri.jarvisalo@gmail.com>
+ */
+class DediReport extends Window
+{
+    protected $textedit, $infolabel, $frame;
+
+    protected function onConstruct()
+    {
+        parent::onConstruct();
+
+        $this->frame = new \ManiaLive\Gui\Controls\Frame();
+        $this->frame->setLayout(new Column());
+        $this->addComponent($this->frame);
+
+        $info = new Label(100, 16);
+        $info->setTextSize(2);
+        $info->setText("To report invalid records for dedimania, \n please copy the following info at clipboard (ctrl-a + ctrl-c)");
+        $this->frame->addComponent($info);
+
+        $this->textedit = new TextEdit("", 100, 40);
+        $this->frame->addComponent($this->textedit);
+
+        $info = new Label(100, 32);
+        $info->setTextSize(2);
+        $info->setText('$fffThen go click following link:$3af'."\n".'$lhttp://dedimania.net/SITE/forum/viewtopic.php?id=384$l '."\n".'$fffand post this information there.');
+        $this->frame->addComponent($info);
+    }
+
+    public function setLogin($login)
+    {
+        $text = "Login to check: ".$login."\n";
+        $text .= "Map: ".Storage::getInstance()->currentMap->uId."\n";
+        $text .= "Reason: *edit your reason here*"."\n";
+        $text .= "Reportee: ".$this->getRecipient()."\n";
+        $this->textedit->setText($text);
+    }
+}
--- Dedimania/Gui/Windows/Records.php
+++ Dedimania/Gui/Windows/Records.php
@@ -9,8 +9,8 @@ use ManiaLivePlugins\eXpansion\LocalRecords\LocalRecords;
 class Records extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
 {
     protected $frame;
-    protected $label_rank, $label_nick, $label_score;
-    protected $widths = array(1, 6, 6);
+    protected $label_rank, $label_nick, $label_login, $label_score;
+    protected $widths = array(1, 3, 4, 4, 1);
     protected $pager;
     protected $items  = array();
     protected $label_visit;
@@ -35,20 +35,22 @@ class Records extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
 
         $this->label_rank = new \ManiaLib\Gui\Elements\Label($scaledSizes[0], 6);
         $this->label_rank->setAlign('left', 'center');
-
         $this->frame->addComponent($this->label_rank);
 
-        $this->label_nick = new \ManiaLib\Gui\Elements\Label($scaledSizes[1], 6);
-        $this->label_nick->setAlign('left', 'center');
-
-        $this->frame->addComponent($this->label_nick);
-        $this->label_score = new \ManiaLib\Gui\Elements\Label($scaledSizes[2], 6);
+        $this->label_score = new \ManiaLib\Gui\Elements\Label($scaledSizes[1], 6);
         $this->label_score->setAlign('left', 'center');
-
         $this->frame->addComponent($this->label_score);
 
+        $this->label_nick = new \ManiaLib\Gui\Elements\Label($scaledSizes[2], 6);
+        $this->label_nick->setAlign('left', 'center');
+        $this->frame->addComponent($this->label_nick);
+
+        $this->label_login = new \ManiaLib\Gui\Elements\Label($scaledSizes[3], 6);
+        $this->label_login->setAlign('left', 'center');
+        $this->frame->addComponent($this->label_login);
+
         $this->button_cps = new \ManiaLivePlugins\eXpansion\Gui\Elements\Button(30, 5);
-        $this->button_cps->setText("Cp Times");
+        $this->button_cps->setText(__("Checkpoints", $login));
         $this->button_cps->setAction(\ManiaLivePlugins\eXpansion\Dedimania\DedimaniaAbstract::$actionOpenCps);
         $this->mainFrame->addComponent($this->button_cps);
 
@@ -63,8 +65,10 @@ class Records extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
         $scaledSizes = Gui::getScaledSize($this->widths, ($this->getSizeX()) - 5);
 
         $this->label_rank->setSizeX($scaledSizes[0]);
-        $this->label_nick->setSizeX($scaledSizes[1]);
-        $this->label_score->setSizeX($scaledSizes[2]);
+        $this->label_score->setSizeX($scaledSizes[1]);
+        $this->label_nick->setSizeX($scaledSizes[2]);
+        $this->label_login->setSizeX($scaledSizes[3]);
+
         $this->button_cps->setPosition($this->getSizeX() - 30, -$this->getSizeY() + 6);
         $this->label_visit->setPosition($this->getSizeX() - 72, -$this->getSizeY() + 6);
 
@@ -77,6 +81,7 @@ class Records extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
     {
         $this->label_rank->setText(__(LocalRecords::$txt_rank, $this->getRecipient()));
         $this->label_nick->setText(__(LocalRecords::$txt_nick, $this->getRecipient()));
+        $this->label_login->setText(__(LocalRecords::$txt_login, $this->getRecipient()));
         $this->label_score->setText(__(LocalRecords::$txt_score, $this->getRecipient()));
     }
 
@@ -107,7 +112,6 @@ class Records extends \ManiaLivePlugins\eXpansion\Gui\Windows\Window
     {
         $this->label_visit->setUrl($url);
         $this->label_visit->setScriptEvents();
-        
     }
 }
 ?>
