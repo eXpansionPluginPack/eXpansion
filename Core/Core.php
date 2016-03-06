27f87c40734743b6f313cd61d4639520c9b6de44
oliverde8 <oliverde8@gmail.com>
2016-03-06 14:35:14 +0100

    Updated eXpansion version

--- Core/Core.php
+++ Core/Core.php
@@ -22,7 +22,7 @@ use Maniaplanet\DedicatedServer\Structures\ServerOptions;
 class Core extends types\ExpPlugin
 {
 
-	const EXP_VERSION = "0.9.8.7";
+	const EXP_VERSION = "1.0.0.0";
 
 	const EXP_REQUIRE_MANIALIVE = "4.0.0";
 
