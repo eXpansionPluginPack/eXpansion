8264e56ab20bbfa2ae4c273affb4b675759b88da
viokCircus <ririlepeintre@yahoo.fr>
2016-03-29 16:53:59 +0200

    Update Config.php

--- Maps/Config.php
+++ Maps/Config.php
@@ -6,6 +6,7 @@ class Config extends \ManiaLib\Utils\Singleton {
 
     public $bufferSize = 5;
     public $historySize = 7;
+    public $showCurrentMapWidget = true;
     public $showNextMapWidget = true;
     public $showEndMatchNotices = true;
     public $publicQueueAmount = array(0);
